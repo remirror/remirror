6ddbd9bfbb747ecb6270e4dca7840c7cc0ef2396
Ifiok Jr <ifiokotung@gmail.com>
2019-02-03 02:59:05 +0000

    test(e2e): add first test for cypress
    - Configure cypress for reporting code coverage stats

--- @remirror/remirror/.storybook/webpack.config.js
+++ @remirror/remirror/.storybook/webpack.config.js
@@ -2,12 +2,24 @@ const path = require('path');
 
 module.exports = (baseConfig, env, config) => {
   config.module.rules.push({
-    test: /\.(ts|tsx)$/,
+    test: /\.tsx?$/,
     loader: require.resolve('babel-loader'),
   });
 
+  if (process.env.COVERAGE === 'true') {
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      use: {
+        loader: 'istanbul-instrumenter-loader',
+        options: { esModules: true },
+      },
+      enforce: 'post',
+      exclude: /node_modules|internal|docs|support|\.(spec|test)\.tsx?$/,
+      include: path.resolve(__dirname, '../../..'),
+    });
+  }
+
   config.resolve.alias = config.resolve.alias || {};
-  config.resolve.alias.react = path.resolve(__dirname, '../../../', './node_modules/react');
   config.externals = { ...(config.externals || {}), fs: '__NOT_USED__' };
 
   config.resolve.extensions.push('.ts', '.tsx');
