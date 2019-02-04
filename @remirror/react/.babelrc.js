1474c3f1196a8d87bd0c13cfa5bfc3d2c3b84bdc
Ifiok Jr <ifiokotung@gmail.com>
2019-02-04 05:26:12 +0000

    fix: update to babel config for better jest configuration

--- @remirror/react/.babelrc.js
+++ @remirror/react/.babelrc.js
@@ -1,17 +1,1 @@
-const defaultConfig = require('../../support/babel/base.babel');
-
-module.exports = {
-  ...defaultConfig,
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        modules: false,
-        targets: {
-          node: '8',
-        },
-      },
-    ],
-  ],
-  plugins: [...defaultConfig.plugins],
-};
+module.exports = require('../../support/babel/base.babel');
