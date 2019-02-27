e85b3300bf73b67fd657de59eb0445173cfbd265
Ifiok Jr <ifiokotung@gmail.com>
2019-02-27 14:18:22 +0000

    feat: add support for custom settings in history configuration

--- @remirror/extensions-core/src/extensions/history.ts
+++ @remirror/extensions-core/src/extensions/history.ts
@@ -1,8 +1,31 @@
 import { CommandFunction, Extension } from '@remirror/core';
 import { history, redo, undo } from 'prosemirror-history';
 
-export class History extends Extension {
-  public name = 'history';
+export interface HistoryOptions {
+  /**
+   * The amount of history events that are collected before the
+   * oldest events are discarded. Defaults to 100.
+   */
+  depth?: number | null;
+
+  /**
+   * The delay between changes after which a new group should be
+   * started. Defaults to 500 (milliseconds). Note that when changes
+   * aren't adjacent, a new group is always started.
+   */
+  newGroupDelay?: number | null;
+}
+export class History extends Extension<HistoryOptions> {
+  get name(): 'history' {
+    return 'history';
+  }
+
+  get defaultOptions() {
+    return {
+      depth: 100,
+      newGroupDelay: 500,
+    };
+  }
 
   public keys() {
     const isMac = typeof navigator !== 'undefined' ? /Mac/.test(navigator.platform) : false;
@@ -20,7 +43,8 @@ export class History extends Extension {
   }
 
   public plugins() {
-    return [history()];
+    const { depth, newGroupDelay } = this.options;
+    return [history({ depth, newGroupDelay })];
   }
 
   public commands() {
