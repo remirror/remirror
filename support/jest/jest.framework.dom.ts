3fe8d7291d25484860b80bc2be80476bd1e041e7
Ifiok Jr <ifiokotung@gmail.com>
2019-02-27 23:21:59 +0000

    test: start to improve testing infra

--- support/jest/jest.framework.dom.ts
+++ support/jest/jest.framework.dom.ts
@@ -4,10 +4,6 @@ import 'react-testing-library/cleanup-after-each';
 
 expect.extend(toHaveNoViolations);
 
-if (process.env.TEST_ENV) {
-  jest.setTimeout(20000);
-}
-
 /* To fix Prosemirror tests in jsdom */
 document.getSelection = () => {
   return {
@@ -15,3 +11,33 @@ document.getSelection = () => {
     removeAllRanges: () => {},
   } as Selection;
 };
+
+// Copied from react-beautiful-dnd/test/setup.js
+if (typeof document !== 'undefined') {
+  // overriding these properties in jsdom to allow them to be controlled
+  Object.defineProperties(document.documentElement, {
+    clientWidth: {
+      writable: true,
+      value: document.documentElement.clientWidth,
+    },
+    clientHeight: {
+      writable: true,
+      value: document.documentElement.clientHeight,
+    },
+    scrollWidth: {
+      writable: true,
+      value: document.documentElement.scrollWidth,
+    },
+    scrollHeight: {
+      writable: true,
+      value: document.documentElement.scrollHeight,
+    },
+  });
+}
+
+// Setting initial viewport
+// Need to set clientWidth and clientHeight as jsdom does not set these properties
+if (typeof document !== 'undefined' && typeof window !== 'undefined') {
+  (document.documentElement as any).clientWidth = window.innerWidth;
+  (document.documentElement as any).clientHeight = window.innerHeight;
+}
--- support/jest/jest.framework.ts
+++ support/jest/jest.framework.ts
@@ -1,7 +1,20 @@
 import 'jest-extended';
 
+import { configureToMatchImageSnapshot } from 'jest-image-snapshot';
+
 if (process.env.TEST_ENV) {
-  jest.setTimeout(60000);
+  jest.setTimeout(600000);
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000;
+
+  // A failureThreshold of 1 will pass tests that have > 2 percent failing pixels
+  const customConfig = { threshold: 0.3 };
+  const toMatchProdImageSnapshot = configureToMatchImageSnapshot({
+    customDiffConfig: customConfig,
+    failureThreshold: 3800,
+    failureThresholdType: 'pixel',
+  });
+
+  expect.extend({ toMatchProdImageSnapshot });
 }
 
 /* Make unhandledRejection errors easier to debug */
@@ -9,3 +22,20 @@ if (process.env.TEST_ENV) {
 process.on('unhandledRejection', reason => {
   console.error('REJECTION', reason);
 });
+
+if (process.env.CI) {
+  const consoleError = console.error;
+  const consoleWarn = console.warn;
+  const consoleLog = console.log;
+  beforeAll(() => {
+    console.error = jest.fn();
+    console.warn = jest.fn();
+    console.log = jest.fn();
+  });
+
+  afterAll(() => {
+    console.error = consoleError;
+    console.warn = consoleWarn;
+    console.log = consoleLog;
+  });
+}
