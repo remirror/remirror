140d35d289aa64a019edb577550cbc11318f88d9
Ifiok Jr <ifiokotung@gmail.com>
2019-03-01 03:32:06 +0000

    refactor: remove useless types

--- support/types/jest__types/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '@jest/types' {
-  import * as Config from './Config';
-  import * as Console from './Console';
-  import * as Global from './Global';
-  import * as Matchers from './Matchers';
-  import * as SourceMaps from './SourceMaps';
-  import * as TestResult from './TestResult';
-  export { Config, Console, Matchers, SourceMaps, TestResult, Global };
-}
