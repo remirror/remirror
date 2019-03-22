5c5636df0a655671f3946216333200c2db9aae46
Ifiok Jr <ifiokotung@gmail.com>
2019-03-22 11:14:15 +0000

    fix: add `@babel/plugin-transform-runtime`
    We need to inject babel-runtime code into the build.

--- support/babel/base.babel.js
+++ support/babel/base.babel.js
@@ -12,6 +12,7 @@ module.exports = {
   presets: [['@babel/preset-env'], '@babel/preset-typescript', '@babel/preset-react'],
   plugins: [
     '@babel/plugin-transform-typescript', // This is needed so that abstract classes are properly compiled
+    ['@babel/plugin-transform-runtime', { corejs: 2 }],
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-syntax-dynamic-import',
