682eb04b3d4a43bf6d74a985fa502532227890e5
Ifiok Jr <ifiokotung@gmail.com>
2019-06-19 07:42:28 +0100

    test: add image diff testing
    Add tests that ensure styles for ssr and dom editors are identical.

--- changelog.md
+++ changelog.md
@@ -11,6 +11,7 @@ and this project will adhere to [Semantic Versioning](https://semver.org/spec/v2
 
 - **BREAKING `remirror/react-utils`:** Rename `childIsFunction` to `propIsFunction` and make it a _pseudo_ predicate function (returns true when it doesn't throw an error).
 - Add support for [Git Large File Storage (LFS)](https://git-lfs.github.com/)
+- `remirror/editor-twitter`, `remirror/editor-wysiwyg` : Use image-snapshot testing to ensure SSR and DOM rendered editors are identical
 
 ## [0.2.0] - 2019-06-18
 
--- e2e/twitter.puppeteer.ts
+++ e2e/twitter.puppeteer.ts
@@ -3,18 +3,53 @@ import { innerHtml, outerHtml, sel, textContent } from './helpers';
 
 const editorSelector = '.remirror-editor';
 
-describe('SSR Twitter Showcase', () => {
+describe('Twitter Editor Snapshots', () => {
+  const url = URLDescriptor.twitter[Deployment.Next][1];
+  const ssrIdentifier = 'twitter-editor-ssr';
+  const domIdentifier = 'twitter-editor-dom';
+
   beforeEach(async () => {
     await jestPuppeteer.resetPage();
-    // Set JavaScript to disabled to mimic server side rendering
-    await page.setJavaScriptEnabled(false);
-    await page.goto(URLDescriptor.twitter[Deployment.Next][1]);
   });
 
-  it('should pre render the editor', async () => {
-    // This test checks that the ProsemirrorEditor exists and contains the expected html
-    await expect(outerHtml(editorSelector)).resolves.toBeTruthy();
-    await expect(outerHtml(editorSelector)).resolves.toMatchSnapshot();
+  describe('SSR', () => {
+    beforeEach(async () => {
+      // Set JavaScript to disabled to mimic server side rendering
+      await page.setJavaScriptEnabled(false);
+      await page.goto(url);
+    });
+
+    it('should pre render the editor with consistent html', async () => {
+      // This test checks that the ProsemirrorEditor exists and contains the expected html
+      await expect(outerHtml(editorSelector)).resolves.toBeTruthy();
+      await expect(outerHtml(editorSelector)).resolves.toMatchSnapshot();
+    });
+
+    it('should match its own snapshot', async () => {
+      const image = await page.screenshot();
+      expect(image).toMatchImageSnapshot({ customSnapshotIdentifier: ssrIdentifier });
+    });
+
+    it('should match the dom image snapshot', async () => {
+      const image = await page.screenshot();
+      expect(image).toMatchImageSnapshot({ customSnapshotIdentifier: domIdentifier });
+    });
+  });
+
+  describe('DOM', () => {
+    beforeEach(async () => {
+      await page.goto(url);
+    });
+
+    it('should match its own snapshot', async () => {
+      const image = await page.screenshot();
+      expect(image).toMatchImageSnapshot({ customSnapshotIdentifier: domIdentifier });
+    });
+
+    it('should match the ssr image snapshot', async () => {
+      const image = await page.screenshot();
+      expect(image).toMatchImageSnapshot({ customSnapshotIdentifier: ssrIdentifier });
+    });
   });
 });
 
