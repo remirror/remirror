593e12326907b1c603dcacf3f0f848a4076d4b04
Ifiok Jr <ifiokotung@gmail.com>
2019-06-23 09:59:58 +0100

    chore: cleanup dependencies and lint config

--- support/babel/base.babel.js
+++ support/babel/base.babel.js
@@ -27,7 +27,6 @@ module.exports = {
     ['@babel/plugin-proposal-class-properties'],
     ['@babel/plugin-proposal-object-rest-spread'],
     '@babel/plugin-syntax-dynamic-import',
-    'lodash',
   ],
   env: { production: nonTestEnv, development: nonTestEnv },
 };
