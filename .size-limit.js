f4fb7fe2e4d9ad3a8f41d4b5d927aae3a005eba7
Ifiok Jr <ifiokotung@gmail.com>
2019-07-17 16:41:07 +0100

    ci: temporarily increase size limitations
    There are several bloated dependencies to remove once
    I have more time. For now just bump up the limit.

--- .size-limit.js
+++ .size-limit.js
@@ -2,6 +2,9 @@ const { join } = require('path');
 
 const core = require('./@remirror/core/package.json');
 const coreExtensions = require('./@remirror/core-extensions/package.json');
+const editorTwitter = require('./@remirror/editor-twitter/package.json');
+const editorWysiwyg = require('./@remirror/editor-wysiwyg/package.json');
+const extensionCodeBlock = require('./@remirror/extension-code-block/package.json');
 const extensionCollaboration = require('./@remirror/extension-collaboration/package.json');
 const extensionEmoji = require('./@remirror/extension-emoji/package.json');
 const extensionEpicMode = require('./@remirror/extension-epic-mode/package.json');
@@ -12,29 +15,29 @@ const reactSSR = require('./@remirror/react-ssr/package.json');
 const reactUtils = require('./@remirror/react-utils/package.json');
 const rendererReact = require('./@remirror/renderer-react/package.json');
 const showcase = require('./@remirror/showcase/package.json');
-const editorTwitter = require('./@remirror/editor-twitter/package.json');
-const editorWysiwyg = require('./@remirror/editor-wysiwyg/package.json');
 
 const limits = {
-  '@remirror/core': '50 KB',
-  '@remirror/core-extensions': '90 KB',
-  '@remirror/extension-collaboration': '50 KB',
-  '@remirror/extension-emoji': '200 KB',
-  '@remirror/extension-epic-mode': '50 KB',
-  '@remirror/extension-enhanced-link': '60 KB',
-  '@remirror/extension-mention': '70 KB',
-  '@remirror/react': '110 KB',
-  '@remirror/react-ssr': '80 KB',
-  '@remirror/react-utils': '50 KB',
-  '@remirror/renderer-react': '60 KB',
+  '@remirror/core': '60 KB',
+  '@remirror/core-extensions': '100 KB',
+  '@remirror/editor-twitter': '250 KB',
+  '@remirror/editor-wysiwyg': '400 KB',
+  '@remirror/extension-code-block': '110 KB',
+  '@remirror/extension-collaboration': '60 KB',
+  '@remirror/extension-emoji': '220 KB',
+  '@remirror/extension-epic-mode': '60 KB',
+  '@remirror/extension-enhanced-link': '70 KB',
+  '@remirror/extension-mention': '80 KB',
+  '@remirror/react': '120 KB',
+  '@remirror/react-ssr': '90 KB',
+  '@remirror/react-utils': '60 KB',
+  '@remirror/renderer-react': '70 KB',
   '@remirror/showcase': '400 KB',
-  '@remirror/editor-twitter': '225 KB',
-  '@remirror/editor-wysiwyg': '350 KB',
 };
 
 module.exports = [
   core,
   coreExtensions,
+  extensionCodeBlock,
   extensionCollaboration,
   extensionEmoji,
   extensionEpicMode,
