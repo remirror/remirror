09fb27f603e19a3781cdae12b5619b4ee3efabad
H. Charley Bodkin <hcbodkin@gmail.com>
2019-07-20 00:41:36 +0000

    add a constants.spec.ts test file

---
+++ @remirror/core-extensions/src/__tests__/constants.spec.ts
@@ -0,0 +1,24 @@
+import {
+  ALL_EXTENSIONS,
+  ALL_GENERIC_EXTENSIONS,
+  ALL_MARK_EXTENSIONS,
+  ALL_NODE_EXTENSIONS,
+} from '../constants';
+
+describe('constants', () => {
+  it('has correctly exported _all_ extensions', () => {
+    expect(ALL_EXTENSIONS).toHaveLength(6);
+  });
+
+  it('has correctly exported _generic_ extensions', () => {
+    expect(ALL_GENERIC_EXTENSIONS).toHaveLength(6);
+  });
+
+  it('has correctly exported _mark_ extensions', () => {
+    expect(ALL_MARK_EXTENSIONS).toHaveLength(6);
+  });
+
+  it('has correctly exported _node_ extensions', () => {
+    expect(ALL_NODE_EXTENSIONS).toHaveLength(6);
+  });
+});
