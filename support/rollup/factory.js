17ce674d3a5d93f5828d130edbc7ef9cd422c347
Ifiok Jr <ifiokotung@gmail.com>
2019-07-21 03:38:52 +0100

    fix: resolve rollup @emotion/core bundle issue
    The babel configuration used for this project automatically injects
    `jsx` from @emotion/core as a replacement for React.createElement.
    
    This was causing problems with rollup which didn't see `@emotion/core`
    as part of the dependencies and as a result would treat emotion as a
    dependency and bundle it with the rest of the code.
    
    Since emotion was bundled it meant that code consuming this library
    could bundle emotion for node / browser and had to stick with the
    browser bundle which this rollup build defaults to.
    
    By adding @emotion/core as a dependency to any package which uses jsx,
    rollup can now safely exclude @emotion/core from the bundle and the
    downstream bug is fixed.

--- support/rollup/factory.js
+++ support/rollup/factory.js
@@ -3,23 +3,22 @@ import builtins from 'rollup-plugin-node-builtins';
 import globals from 'rollup-plugin-node-globals';
 import json from 'rollup-plugin-json';
 import resolve from 'rollup-plugin-node-resolve';
+import autoExternal from 'rollup-plugin-auto-external';
+import path from 'path';
 
 /* Inspired by https://github.com/ianstormtaylor/slate/blob/6302b2d9d2d6a62ba32e6a7d987db5db8f846eef/support/rollup/factory.js#L1-L148 */
 
 /**
- * Return a Rollup configuration for a `pkg` with `env` and `target`.
+ * Return a Rollup configuration for a `pkg`
  *
  * @param {Object} pkg
- * @param {String} env
- * @param {String} format
  * @return {Object}
  */
 
-function configure(pkg, env, target, rootFolder = '@remirror') {
+function configure(pkg, rootFolder = '@remirror') {
   const folderName = pkg.name.replace('@remirror/', '');
   const extensions = ['.mjs', '.json', '.ts', '.tsx', '.js'];
 
-  const isModule = target === 'module';
   const input = `${rootFolder}/${folderName}/src/index.ts`;
   const deps = []
     .concat(pkg.dependencies ? Object.keys(pkg.dependencies) : [])
@@ -48,35 +47,45 @@ function configure(pkg, env, target, rootFolder = '@remirror') {
       runtimeHelpers: true,
     }),
 
+    autoExternal({
+      dependencies: true,
+      packagePath: path.resolve(
+        __dirname,
+        '../../',
+        rootFolder,
+        folderName,
+        'package.json',
+      ),
+      peerDependencies: true,
+    }),
+
     // Register Node.js globals for browserify compatibility.
     globals(),
-  ].filter(Boolean);
+  ];
 
-  if (isModule) {
-    return {
-      plugins,
-      input,
-      output: [
-        {
-          file: `${rootFolder}/${folderName}/${pkg.module}`,
-          format: 'es',
-          sourcemap: true,
-        },
-        {
-          file: `${rootFolder}/${folderName}/${pkg.cjs}`,
-          format: 'cjs',
-          exports: 'named',
-          sourcemap: true,
-        },
-      ],
-      // We need to explicitly state which modules are external, meaning that
-      // they are present at runtime. In the case of non-UMD configs, this means
-      // all non-Remirror packages.
-      external: id => {
-        return !!deps.find(dep => dep === id || id.startsWith(`${dep}/`));
+  return {
+    plugins,
+    input,
+    output: [
+      {
+        file: `${rootFolder}/${folderName}/${pkg.module}`,
+        format: 'es',
+        sourcemap: true,
       },
-    };
-  }
+      {
+        file: `${rootFolder}/${folderName}/${pkg.cjs}`,
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true,
+      },
+    ],
+    // We need to explicitly state which modules are external, meaning that
+    // they are present at runtime. In the case of non-UMD configs, this means
+    // all non-Remirror packages.
+    external: id => {
+      return !!deps.find(dep => dep === id || id.startsWith(`${dep}/`));
+    },
+  };
 }
 
 /**
@@ -86,7 +95,7 @@ function configure(pkg, env, target, rootFolder = '@remirror') {
  */
 
 function factory(pkg, rootFolder) {
-  return [configure(pkg, 'development', 'module', rootFolder)].filter(Boolean);
+  return [configure(pkg, rootFolder)];
 }
 
 /**
