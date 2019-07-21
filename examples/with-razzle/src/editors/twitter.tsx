8c732cb719dfe4e329c7399cf4bcc0662e58e456
Ifiok Jr <ifiokotung@gmail.com>
2019-07-21 02:34:47 +0100

    chore: add razzlejs example

---
+++ examples/with-razzle/src/editors/twitter.tsx
@@ -0,0 +1,6 @@
+import { ShowcaseTwitterEditor } from '@remirror/showcase/lib/twitter';
+import React from 'react';
+
+import '../home.css';
+
+export default () => <ShowcaseTwitterEditor suppressHydrationWarning={true} />;
---
+++ examples/with-razzle/src/home.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+
+import './home.css';
+
+class Home extends React.Component<{}, {}> {
+  public render() {
+    return (
+      <div className='Home'>
+        <div className='Home-header'>
+          <img src={logo} className='Home-logo' alt='logo' />
+          <h2>Welcome to Razzles / Remirror</h2>
+        </div>
+        <p className='Home-intro'>
+          To get started, edit <code>src/App.tsx</code> or <code>src/Home.tsx</code> and save to reload.
+        </p>
+        <ul className='Home-resources'>
+          <li>
+            <a href='/wysiwyg'>wysiwyg</a>
+          </li>
+          <li>
+            <a href='/twitter'>twitter</a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Home;
