998966da18dcb1c0f95e43aa13d33dbf64643e8d
Ifiok Jr <ifiokotung@gmail.com>
2019-07-21 06:40:19 +0100

    chore: cleanup razzle code
    Support SSR with no flicker.

--- examples/with-razzle/src/editors/twitter.tsx
+++ examples/with-razzle/src/editors/twitter.tsx
@@ -1,6 +1,4 @@
 import { ShowcaseTwitterEditor } from '@remirror/showcase/lib/twitter';
 import React from 'react';
 
-import '../home.css';
-
 export default () => <ShowcaseTwitterEditor suppressHydrationWarning={true} />;
--- examples/with-razzle/src/editors/wysiwyg.tsx
+++ examples/with-razzle/src/editors/wysiwyg.tsx
@@ -1,8 +1,6 @@
 import { ExampleWysiwygEditor, WYSIWYG_SHOWCASE_CONTENT } from '@remirror/showcase/lib/wysiwyg';
 import React from 'react';
 
-import '../home.css';
-
 export default () => (
   <ExampleWysiwygEditor initialContent={WYSIWYG_SHOWCASE_CONTENT} suppressHydrationWarning={true} />
 );
