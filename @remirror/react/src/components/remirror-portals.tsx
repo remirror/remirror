cfdb0470dfb7dea98b86d759e3e15d729cf14ff1
Ifiok Jr <ifiokotung@gmail.com>
2019-07-30 18:25:22 +0100

    feat: auto delete portals on container removal
    This allows for decorations widgets to use portals.

--- @remirror/react/src/components/remirror-portals.tsx
+++ @remirror/react/src/components/remirror-portals.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 
 import { PortalContainer, PortalMap } from '@remirror/core';
@@ -12,6 +12,9 @@ export interface RemirrorPortalsProps {
 
 /**
  * The component that places all the portals into the DOM.
+ *
+ * Portals can be created by NodeView and also the static
+ * widget method on Decorations.
  */
 export const RemirrorPortals = ({ portalContainer }: RemirrorPortalsProps) => {
   const [state, setState] = useState(Array.from(portalContainer.portals.entries()));
@@ -30,9 +33,44 @@ export const RemirrorPortals = ({ portalContainer }: RemirrorPortalsProps) => {
 
   return (
     <>
-      {state.map(([container, { render: Component, key }]) => (
-        <Fragment key={key}>{createPortal(<Component />, container)}</Fragment>
-      ))}
+      {state.map(([container, { render: Component, key }]) =>
+        createPortal(
+          <Portal container={container} Component={Component} portalContainer={portalContainer} />,
+          container,
+          key,
+        ),
+      )}
     </>
   );
+};
+
+export interface PortalProps extends RemirrorPortalsProps {
+  /**
+   * Holds the element that this portal is being rendered into.
+   */
+  container: HTMLElement;
+
+  /**
+   * The plain component to render.
+   */
+  Component: () => JSX.Element;
+}
+
+/**
+ * This is the component rendered by the createPortal method within the
+ * RemirrorPortals component. It is responsible for cleanup when the container
+ * is removed from the DOM.
+ */
+const Portal = ({ portalContainer, container, Component }: PortalProps) => {
+  useEffect(() => {
+    /**
+     * Remove the portal container entry when this portal is unmounted.
+     * Portals are unmounted when their host container is removed from the dom.
+     */
+    return () => {
+      portalContainer.remove(container);
+    };
+  }, [portalContainer]);
+
+  return <Component />;
 };
