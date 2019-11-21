9c262e16e510dbc67bf2a3b1a9ebc8aa86d473c4
Ifiok Jr <ifiokotung@gmail.com>
2019-11-21 16:48:45 +0000

    ci: enable caching of node_modules in builds (#177)
    * ci: enable caching of node_modules in builds
    
    * chore: upgrade dependencies

---
+++ support/scripts/md.js
@@ -0,0 +1,23 @@
+const { mkdirSync, statSync } = require('fs');
+
+const [, , arg] = process.argv;
+
+if (!arg) {
+  console.error('Please provide a directory name to create.');
+  process.exit(1);
+}
+
+const directory = arg;
+
+const directoryExists = path => {
+  try {
+    return statSync(path).isDirectory;
+  } catch {
+    return false;
+  }
+};
+
+if (!directoryExists(directory)) {
+  console.log(`Creating directory for caching: ${directory}`);
+  mkdirSync(directory);
+}
