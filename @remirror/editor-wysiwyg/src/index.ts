1d5b6231f6a1402ae0efcedb201640bf7eddf8ca
Hugh Boylan <bluehugh2@gmail.com>
2020-02-04 18:57:01 -0600

    export wysiwyg components

--- @remirror/editor-wysiwyg/src/index.ts
+++ @remirror/editor-wysiwyg/src/index.ts
@@ -1,3 +1,5 @@
+export * from './components/wysiwyg-components';
 export * from './components/wysiwyg-editor';
+export * from './components/wysiwyg-menu';
 export * from './wysiwyg-types';
 export * from './wysiwyg-theme';
