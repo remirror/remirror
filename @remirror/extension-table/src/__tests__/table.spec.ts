67b158dbbd1e9354c19c63a79904da836c98228d
ocavue <ocavue@gmail.com>
2020-02-15 14:54:53 +0800

    a

--- @remirror/extension-table/src/__tests__/table.spec.ts
+++ @remirror/extension-table/src/__tests__/table.spec.ts
@@ -2,55 +2,55 @@
  * @jest-environment node
  */
 
-import { ObjectNode } from '@remirror/core';
-import { renderSSREditor } from 'jest-remirror';
-import javascript from 'refractor/lang/javascript';
-import markdown from 'refractor/lang/markdown';
-import typescript from 'refractor/lang/typescript';
+// import { ObjectNode } from '@remirror/core';
+// import { renderSSREditor } from 'jest-remirror';
+// import javascript from 'refractor/lang/javascript';
+// import markdown from 'refractor/lang/markdown';
+// import typescript from 'refractor/lang/typescript';
 
-import { CodeBlockExtension } from '..';
+// import { CodeBlockExtension } from '..';
 
-const supportedLanguages = [typescript, javascript, markdown];
-const create = (initialContent: ObjectNode) =>
-  renderSSREditor([new CodeBlockExtension({ supportedLanguages })], { initialContent });
+// const supportedLanguages = [typescript, javascript, markdown];
+// const create = (initialContent: ObjectNode) =>
+//   renderSSREditor([new CodeBlockExtension({ supportedLanguages })], { initialContent });
 
-test('ssr component', () => {
-  const reactString = create({
-    type: 'doc',
-    content: [
-      {
-        type: 'codeBlock',
-        content: [
-          {
-            type: 'text',
-            text:
-              'Simple Code Blocks\necho "fun times"\nUse Shift-Enter or Mod-Enter to hard break out of the code block',
-          },
-        ],
-      },
-    ],
-  });
+// test('ssr component', () => {
+//   const reactString = create({
+//     type: 'doc',
+//     content: [
+//       {
+//         type: 'codeBlock',
+//         content: [
+//           {
+//             type: 'text',
+//             text:
+//               'Simple Code Blocks\necho "fun times"\nUse Shift-Enter or Mod-Enter to hard break out of the code block',
+//           },
+//         ],
+//       },
+//     ],
+//   });
 
-  expect(reactString).toInclude('<pre class="language-markup"><code>Simple Code Blocks');
-  expect(reactString).toMatchSnapshot();
-});
+//   expect(reactString).toInclude('<pre class="language-markup"><code>Simple Code Blocks');
+//   expect(reactString).toMatchSnapshot();
+// });
 
-test('formatted ssr component', () => {
-  const reactString = create({
-    type: 'doc',
-    content: [
-      {
-        type: 'codeBlock',
-        attrs: { language: 'markdown' },
-        content: [
-          {
-            type: 'text',
-            text: '# Welcome\n**To the greatest show**\n\n_Ever created_.',
-          },
-        ],
-      },
-    ],
-  });
+// test('formatted ssr component', () => {
+//   const reactString = create({
+//     type: 'doc',
+//     content: [
+//       {
+//         type: 'codeBlock',
+//         attrs: { language: 'markdown' },
+//         content: [
+//           {
+//             type: 'text',
+//             text: '# Welcome\n**To the greatest show**\n\n_Ever created_.',
+//           },
+//         ],
+//       },
+//     ],
+//   });
 
-  expect(reactString).toMatchSnapshot();
-});
+//   expect(reactString).toMatchSnapshot();
+// });
