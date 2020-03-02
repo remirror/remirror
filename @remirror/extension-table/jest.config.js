d189c0fafbf23ec4efc16e7c11cd8b559315e798
ocavue <ocavue@gmail.com>
2020-03-02 23:04:32 +0800

    tests

--- @remirror/extension-table/jest.config.js
+++ @remirror/extension-table/jest.config.js
@@ -4,4 +4,9 @@ module.exports = {
   ...config,
   name: '@remirror/extension-table',
   displayName: 'extension-table',
+  testEnvironment: 'enzyme',
+  setupFilesAfterEnv: [...config.setupFilesAfterEnv, 'jest-enzyme'],
+  testEnvironmentOptions: {
+    enzymeAdapter: 'react16',
+  },
 };
--- @remirror/extension-table/src/__tests__/table.spec.tsx
+++ @remirror/extension-table/src/__tests__/table.spec.tsx
@@ -1,8 +1,12 @@
-import { fromHTML, toHTML } from '@remirror/core';
+/** @jsx jsx */
+
+import { jsx } from '@emotion/core';
+import { ProsemirrorNode, fromHTML, toHTML } from '@remirror/core';
 import { BaseKeymapExtension } from '@remirror/core-extensions';
 import { createBaseTestManager } from '@remirror/test-fixtures';
+import { shallow } from 'enzyme';
 import { pmBuild } from 'jest-prosemirror';
-import { renderEditor } from 'jest-remirror';
+import { TaggedProsemirrorNode, renderEditor } from 'jest-remirror';
 import typescriptPlugin from 'prettier/parser-typescript';
 import { formatWithCursor } from 'prettier/standalone';
 import javascript from 'refractor/lang/javascript';
@@ -12,16 +16,6 @@ import typescript from 'refractor/lang/typescript';
 
 import { TableCellExtension, TableExtension, TableRowExtension } from '..';
 
-
-/**
- *
- * @param args A
- */
-const html = (strings: TemplateStringsArray) => {
-  return strings[0]
-}
-
-
 describe('schema', () => {
   const { schema } = createBaseTestManager([
     { extension: new TableExtension() },
@@ -29,22 +23,40 @@ describe('schema', () => {
     { extension: new TableCellExtension() },
   ]);
 
-  const { table, tableRow, tableCell } = pmBuild(schema, {
+  const { doc, table, tableRow, tableCell } = pmBuild(schema, {
     table: { nodeType: 'table' },
     tableRow: { nodeType: 'tableRow' },
     tableCell: { nodeType: 'tableCell' },
   });
 
+  const node = table(
+    tableRow(tableCell('A1'), tableCell('B1')),
+    tableRow(tableCell('A2'), tableCell('B2')),
+    tableRow(tableCell('A3'), tableCell('B3')),
+  );
+
+  const html: string = shallow(
+    <table>
+      <tr>
+        <td>A1</td>
+        <td>B1</td>
+      </tr>
+      <tr>
+        <td>A2</td>
+        <td>B2</td>
+      </tr>
+      <tr>
+        <td>A3</td>
+        <td>B3</td>
+      </tr>
+    </table>,
+  ).html();
+
   it('dump to html', () => {
-    expect(
-      toHTML({
-        node: table(
-          tableRow(tableCell('A1'), tableCell('B1')),
-          tableRow(tableCell('A2'), tableCell('B2')),
-          tableRow(tableCell('A3'), tableCell('B3')),
-        ),
-        schema,
-      }),
-    ).toBe(html`<table><tr><td>A1</td><td>B1</td></tr><tr><td>A2</td><td>B2</td></tr><tr><td>A3</td><td>B3</td></tr></table>`);
+    expect(toHTML({ node, schema })).toBe(html);
+  });
+
+  it('parse from html', () => {
+    expect(fromHTML({ content: html, schema })).toEqualProsemirrorNode(doc(node));
   });
 });
