1e131e07572ec82519b976433177cc296b49d245
Benjie Gillam <benjie@jemjie.com>
2020-03-05 16:22:02 +0000

    Consistent code when entering advanced mode

--- @remirror/playground/src/index.tsx
+++ @remirror/playground/src/index.tsx
@@ -3,9 +3,52 @@ import React, { FC, useCallback, useState } from 'react';
 import CodeEditor from './code-editor';
 import { Container, Header, Main, Panel } from './primitives';
 
+interface CodeOptions {
+  extensions?: Array<{ module: string; export?: string; version: string }>;
+}
+
+function makeCode(codeOptions: CodeOptions): string {
+  const code = `\
+import React from 'react';
+import { BoldExtension, ItalicExtension } from '@remirror/core-extensions';
+import { RemirrorProvider, useExtensionManager, useExtension } from '@remirror/react';
+
+// Set up the component to provide the functionality for the editor
+const SmallEditor = () => {
+  const { getRootProps, commands } = useRemirror(); // Picked from the context.
+
+  return (
+    <div>
+      <button onClick={() => commands.bold()}>bold<button>
+      <button onClick={() => commands.italic()}>italic<button>
+      <div {...getRootProps()} />
+    </div>
+  );
+};
+
+const SmallEditorWrapper = () => {
+  const boldExtension = useExtension(BoldExtension, 2);
+  const italicExtension = useExtension(ItalicExtension, 2);
+
+  const extensionManager = useExtensionManager([italicExtension, boldExtension], {
+    excludeBaseExtensions: false,
+  });
+
+  return (
+    <RemirrorProvider extensionManager={extensionManager}>
+      <SmallEditor />
+    </RemirrorProvider>
+  );
+};
+`;
+  // TODO: prettier
+  return code;
+}
+
 const Playground: FC = () => {
   const [value, setValue] = useState('// Add some code here\n');
   const [advanced, setAdvanced] = useState(false);
+  const [options, setOptions] = useState({} as CodeOptions);
   const handleToggleAdvanced = useCallback(() => {
     if (
       confirm(
@@ -14,9 +57,16 @@ const Playground: FC = () => {
           : "Are you sure you want to enter advanced mode? You'll lose access to the configuration panel",
       )
     ) {
-      setAdvanced(!advanced);
+      if (!advanced) {
+        setAdvanced(true);
+        setValue(makeCode(options));
+      } else {
+        setAdvanced(false);
+      }
     }
-  }, [advanced]);
+  }, [advanced, options]);
+
+  const code = advanced ? value : makeCode(options);
   return (
     <Container>
       <Header>Playground</Header>
@@ -32,7 +82,7 @@ const Playground: FC = () => {
             </>
           )}
         </Panel>
-        <CodeEditor value={value} onChange={setValue} readOnly={!advanced} />
+        <CodeEditor value={code} onChange={setValue} readOnly={!advanced} />
         <Panel flex='1'>
           <div style={{ padding: '1rem' }}>[Insert editor here]</div>
         </Panel>
