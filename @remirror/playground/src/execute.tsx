ca709e6ea64fa20cdecfb932f6e2c23667461a0e
Benjie Gillam <benjie@jemjie.com>
2020-03-26 17:02:02 +0000

    Pull in arbitrary npm modules

--- @remirror/playground/src/execute.tsx
+++ @remirror/playground/src/execute.tsx
@@ -1,8 +1,11 @@
+import * as crypto from 'crypto';
+
 // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 // @ts-ignore
 import * as babelRuntimeHelpersInteropRequireDefault from '@babel/runtime/helpers/interopRequireDefault';
-import React, { FC, useEffect, useRef } from 'react';
+import React, { FC, useCallback, useEffect, useMemo, useRef } from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
+import { debounce } from 'remirror';
 
 import * as remirrorCoreExtensions from '@remirror/core-extensions';
 import * as remirrorReact from '@remirror/react';
@@ -20,6 +23,35 @@ const knownRequires: { [moduleName: string]: any } = {
   react: React,
 };
 
+const fetchedModules: {
+  [id: string]: {
+    name: string;
+    modulePromise: Promise<any>;
+  };
+} = {};
+
+const hash = (str: string): string => {
+  return `_${crypto
+    .createHash('sha1')
+    .update(str)
+    .digest('hex')}`;
+};
+
+function bundle(moduleName: string, id: string): Promise<any> {
+  return new Promise((resolve, reject) => {
+    const el = document.createElement('script');
+    el.onload = () => {
+      console.log(`LOADED ${moduleName}`);
+      resolve((window as any)[id]);
+    };
+    el.onerror = event => {
+      reject(new Error(`Failed to load ${el.src}`));
+    };
+    el.src = `http://bundle.run/${encodeURIComponent(moduleName)}@latest?name=${encodeURIComponent(id)}`;
+    document.body.appendChild(el);
+  });
+}
+
 async function makeRequire(requires: string[]) {
   const tasks: Array<Promise<void>> = [];
   const modules: { [moduleName: string]: any } = {};
@@ -27,7 +59,18 @@ async function makeRequire(requires: string[]) {
     if (knownRequires[moduleName]) {
       modules[moduleName] = knownRequires[moduleName];
     } else {
-      throw new Error('TODO');
+      const id = hash(moduleName);
+      if (!fetchedModules[id]) {
+        fetchedModules[id] = {
+          name: moduleName,
+          modulePromise: bundle(moduleName, id),
+        };
+      }
+      tasks.push(
+        fetchedModules[id].modulePromise.then(remoteModule => {
+          modules[moduleName] = remoteModule;
+        }),
+      );
     }
   }
 
@@ -70,6 +113,7 @@ function runCodeInDiv(div: HTMLDivElement, { code, requires }: { code: string; r
         div,
       );
     } catch (e) {
+      console.error(e);
       render(
         <div>
           <h1>Error occurred</h1>
@@ -91,13 +135,20 @@ function runCodeInDiv(div: HTMLDivElement, { code, requires }: { code: string; r
 export const Execute: FC<ExecuteProps> = props => {
   const { code, requires } = props;
   const ref = useRef<HTMLDivElement | null>(null);
+  const debouncedRunCodeInDiv = useMemo(
+    () =>
+      debounce(500, false, (code: string, requires: string[]) => {
+        if (ref.current) {
+          const release = runCodeInDiv(ref.current, { code, requires });
+          return release;
+        }
+        return;
+      }),
+    [],
+  );
   useEffect(() => {
-    if (ref.current) {
-      const release = runCodeInDiv(ref.current, { code, requires });
-      return release;
-    }
-    return;
-  }, [code, requires]);
+    return debouncedRunCodeInDiv(code, requires);
+  }, [debouncedRunCodeInDiv, code, requires]);
 
   return <div ref={ref} />;
 };
