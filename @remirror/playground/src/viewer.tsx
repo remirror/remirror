ac0591af9b70b9d19225e71e9ca5b7d75a2b1b30
Benjie Gillam <benjie@jemjie.com>
2020-03-26 14:49:14 +0000

    Catch errors during code compiling

--- @remirror/playground/src/viewer.tsx
+++ @remirror/playground/src/viewer.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from 'react';
+import { transform } from '@babel/standalone';
+import React, { FC, useMemo } from 'react';
 
 import { CodeOptions } from './interfaces';
 
@@ -7,13 +8,41 @@ export interface ViewerProps {
   code: string;
 }
 
+function compile(code: string): string {
+  const result = transform(code, {
+    presets: ['env'],
+  });
+  if (result?.code) {
+    return String(result.code);
+  } else {
+    throw new Error('Failed to compile');
+  }
+}
+
 export const Viewer: FC<ViewerProps> = props => {
   const { code } = props;
+  const compiledCode = useMemo(() => {
+    try {
+      return compile(code);
+    } catch (e) {
+      return e;
+    }
+  }, [code]);
+
   return (
     <div style={{ flex: '1 0 0', overflow: 'auto' }}>
-      <pre>
-        <code>{code}</code>
-      </pre>
+      {typeof compiledCode === 'string' ? (
+        <pre>
+          <code>{compiledCode}</code>
+        </pre>
+      ) : (
+        <div>
+          <h1>Error occurred compiling code</h1>
+          <pre>
+            <code>{String(compiledCode)}</code>
+          </pre>
+        </div>
+      )}
     </div>
   );
 };
