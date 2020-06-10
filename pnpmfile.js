0da948932d4cfe24ec34af06b306a8adc36cae32
Ifiok Jr <ifiokotung@gmail.com>
2020-06-10 14:48:16 +0100

    build: update lock files

--- pnpmfile.js
+++ pnpmfile.js
@@ -6,7 +6,7 @@ module.exports = {
 
 function readPackage(pkg) {
   if (pkg.dependencies && pkg.dependencies.graphql) {
-    pkg.dependencies.graphql = '^14';
+    pkg.dependencies.graphql = '^14.6.0';
   }
 
   return pkg;
