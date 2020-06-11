15864dfd5d30d379e3b9ad5f6e74b67e3a9b0925
Benjie Gillam <benjie@jemjie.com>
2020-06-11 13:52:46 +0100

    Format generated file with prettier

--- @remirror/playground/src/_remirror.tsx
+++ @remirror/playground/src/_remirror.tsx
@@ -7,8 +7,13 @@
 \******************************************************************************/
 
 // Remirror custom imports
-import { RemirrorProvider, useExtension, useManager, useRemirror } from '@remirror/react';
-import { useRemirrorPlayground } from './use-remirror-playground';
+import {
+  RemirrorProvider,
+  useExtension,
+  useManager,
+  useRemirror,
+} from "@remirror/react";
+import { useRemirrorPlayground } from "./use-remirror-playground";
 
 export const IMPORT_CACHE: { [moduleName: string]: any } = {
   // Auto-imported
@@ -48,259 +53,198 @@ export const IMPORT_CACHE: { [moduleName: string]: any } = {
   "remirror/preset/table": require("remirror/preset/table"),
 
   // Manual-imported
-  remirror: require('remirror'),
-  'remirror/react': { RemirrorProvider, useManager, useExtension, useRemirror },
-  '@remirror/playground': { useRemirrorPlayground },
+  remirror: require("remirror"),
+  "remirror/react": { RemirrorProvider, useManager, useExtension, useRemirror },
+  "@remirror/playground": { useRemirrorPlayground },
 
   // External dependencies
-  '@babel/runtime/helpers/interopRequireDefault': require('@babel/runtime/helpers/interopRequireDefault'),
-  react: require('react'),
+  "@babel/runtime/helpers/interopRequireDefault": require("@babel/runtime/helpers/interopRequireDefault"),
+  react: require("react"),
 };
 
-export const INTERNAL_MODULES: {moduleName: string, exports: string[]}[] = [
+export const INTERNAL_MODULES: { moduleName: string; exports: string[] }[] = [
   {
-  "moduleName": "remirror/extension/auto-link",
-  "exports": [
-    "AutoLinkExtension"
-  ]
-},
+    moduleName: "remirror/extension/auto-link",
+    exports: ["AutoLinkExtension"],
+  },
   {
-  "moduleName": "remirror/extension/base-keymap",
-  "exports": [
-    "BaseKeymapExtension"
-  ]
-},
+    moduleName: "remirror/extension/base-keymap",
+    exports: ["BaseKeymapExtension"],
+  },
   {
-  "moduleName": "remirror/extension/blockquote",
-  "exports": [
-    "BlockquoteExtension"
-  ]
-},
+    moduleName: "remirror/extension/blockquote",
+    exports: ["BlockquoteExtension"],
+  },
   {
-  "moduleName": "remirror/extension/bold",
-  "exports": [
-    "BoldExtension"
-  ]
-},
+    moduleName: "remirror/extension/bold",
+    exports: ["BoldExtension"],
+  },
   {
-  "moduleName": "remirror/extension/code",
-  "exports": [
-    "CodeExtension"
-  ]
-},
+    moduleName: "remirror/extension/code",
+    exports: ["CodeExtension"],
+  },
   {
-  "moduleName": "remirror/extension/code-block",
-  "exports": [
-    "CodeBlockExtension",
-    "getLanguage"
-  ]
-},
+    moduleName: "remirror/extension/code-block",
+    exports: ["CodeBlockExtension", "getLanguage"],
+  },
   {
-  "moduleName": "remirror/extension/collaboration",
-  "exports": [
-    "CollaborationExtension"
-  ]
-},
+    moduleName: "remirror/extension/collaboration",
+    exports: ["CollaborationExtension"],
+  },
   {
-  "moduleName": "remirror/extension/doc",
-  "exports": [
-    "DocExtension"
-  ]
-},
+    moduleName: "remirror/extension/doc",
+    exports: ["DocExtension"],
+  },
   {
-  "moduleName": "remirror/extension/drop-cursor",
-  "exports": [
-    "DropCursorExtension"
-  ]
-},
+    moduleName: "remirror/extension/drop-cursor",
+    exports: ["DropCursorExtension"],
+  },
   {
-  "moduleName": "remirror/extension/emoji",
-  "exports": [
-    "DEFAULT_FREQUENTLY_USED",
-    "EMOTICONS",
-    "EmojiExtension",
-    "SKIN_VARIATIONS",
-    "aliasToName",
-    "emojiCategories",
-    "emojiList",
-    "emojiNames",
-    "emoticonRegex",
-    "getEmojiByName",
-    "getEmojiFromEmoticon",
-    "getHexadecimalsFromEmoji",
-    "isEmojiAliasName",
-    "isEmojiName",
-    "isValidEmojiName",
-    "isValidEmojiObject",
-    "isValidSkinVariation",
-    "populateFrequentlyUsed",
-    "sortEmojiMatches"
-  ]
-},
+    moduleName: "remirror/extension/emoji",
+    exports: [
+      "DEFAULT_FREQUENTLY_USED",
+      "EMOTICONS",
+      "EmojiExtension",
+      "SKIN_VARIATIONS",
+      "aliasToName",
+      "emojiCategories",
+      "emojiList",
+      "emojiNames",
+      "emoticonRegex",
+      "getEmojiByName",
+      "getEmojiFromEmoticon",
+      "getHexadecimalsFromEmoji",
+      "isEmojiAliasName",
+      "isEmojiName",
+      "isValidEmojiName",
+      "isValidEmojiObject",
+      "isValidSkinVariation",
+      "populateFrequentlyUsed",
+      "sortEmojiMatches",
+    ],
+  },
   {
-  "moduleName": "remirror/extension/epic-mode",
-  "exports": [
-    "COLORS",
-    "EpicModeExtension",
-    "defaultEffect",
-    "heartEffect",
-    "spawningEffect"
-  ]
-},
+    moduleName: "remirror/extension/epic-mode",
+    exports: [
+      "COLORS",
+      "EpicModeExtension",
+      "defaultEffect",
+      "heartEffect",
+      "spawningEffect",
+    ],
+  },
   {
-  "moduleName": "remirror/extension/gap-cursor",
-  "exports": [
-    "GapCursorExtension",
-    "editorStyles",
-    "isGapCursorSelection"
-  ]
-},
+    moduleName: "remirror/extension/gap-cursor",
+    exports: ["GapCursorExtension", "editorStyles", "isGapCursorSelection"],
+  },
   {
-  "moduleName": "remirror/extension/hard-break",
-  "exports": [
-    "HardBreakExtension"
-  ]
-},
+    moduleName: "remirror/extension/hard-break",
+    exports: ["HardBreakExtension"],
+  },
   {
-  "moduleName": "remirror/extension/heading",
-  "exports": [
-    "HeadingExtension"
-  ]
-},
+    moduleName: "remirror/extension/heading",
+    exports: ["HeadingExtension"],
+  },
   {
-  "moduleName": "remirror/extension/history",
-  "exports": [
-    "HistoryExtension"
-  ]
-},
+    moduleName: "remirror/extension/history",
+    exports: ["HistoryExtension"],
+  },
   {
-  "moduleName": "remirror/extension/horizontal-rule",
-  "exports": [
-    "HorizontalRuleExtension"
-  ]
-},
+    moduleName: "remirror/extension/horizontal-rule",
+    exports: ["HorizontalRuleExtension"],
+  },
   {
-  "moduleName": "remirror/extension/image",
-  "exports": [
-    "ImageExtension",
-    "isImageFileType"
-  ]
-},
+    moduleName: "remirror/extension/image",
+    exports: ["ImageExtension", "isImageFileType"],
+  },
   {
-  "moduleName": "remirror/extension/italic",
-  "exports": [
-    "ItalicExtension"
-  ]
-},
+    moduleName: "remirror/extension/italic",
+    exports: ["ItalicExtension"],
+  },
   {
-  "moduleName": "remirror/extension/link",
-  "exports": [
-    "LinkExtension"
-  ]
-},
+    moduleName: "remirror/extension/link",
+    exports: ["LinkExtension"],
+  },
   {
-  "moduleName": "remirror/extension/mention",
-  "exports": [
-    "MentionExtension"
-  ]
-},
+    moduleName: "remirror/extension/mention",
+    exports: ["MentionExtension"],
+  },
   {
-  "moduleName": "remirror/extension/paragraph",
-  "exports": [
-    "ParagraphExtension"
-  ]
-},
+    moduleName: "remirror/extension/paragraph",
+    exports: ["ParagraphExtension"],
+  },
   {
-  "moduleName": "remirror/extension/placeholder",
-  "exports": [
-    "EMPTY_NODE_CLASS_NAME",
-    "EMPTY_NODE_CLASS_SELECTOR",
-    "PlaceholderExtension"
-  ]
-},
+    moduleName: "remirror/extension/placeholder",
+    exports: [
+      "EMPTY_NODE_CLASS_NAME",
+      "EMPTY_NODE_CLASS_SELECTOR",
+      "PlaceholderExtension",
+    ],
+  },
   {
-  "moduleName": "remirror/extension/position-tracker",
-  "exports": [
-    "PositionTrackerExtension"
-  ]
-},
+    moduleName: "remirror/extension/position-tracker",
+    exports: ["PositionTrackerExtension"],
+  },
   {
-  "moduleName": "remirror/extension/positioner",
-  "exports": [
-    "PositionerExtension",
-    "bubblePositioner",
-    "defaultPositioner",
-    "floatingPositioner",
-    "getInitialPosition",
-    "popupMenuPositioner"
-  ]
-},
+    moduleName: "remirror/extension/positioner",
+    exports: [
+      "PositionerExtension",
+      "bubblePositioner",
+      "defaultPositioner",
+      "floatingPositioner",
+      "getInitialPosition",
+      "popupMenuPositioner",
+    ],
+  },
   {
-  "moduleName": "remirror/extension/react-ssr",
-  "exports": [
-    "ReactSSRExtension"
-  ]
-},
+    moduleName: "remirror/extension/react-ssr",
+    exports: ["ReactSSRExtension"],
+  },
   {
-  "moduleName": "remirror/extension/strike",
-  "exports": [
-    "StrikeExtension"
-  ]
-},
+    moduleName: "remirror/extension/strike",
+    exports: ["StrikeExtension"],
+  },
   {
-  "moduleName": "remirror/extension/text",
-  "exports": [
-    "TextExtension"
-  ]
-},
+    moduleName: "remirror/extension/text",
+    exports: ["TextExtension"],
+  },
   {
-  "moduleName": "remirror/extension/trailing-node",
-  "exports": [
-    "TrailingNodeExtension"
-  ]
-},
+    moduleName: "remirror/extension/trailing-node",
+    exports: ["TrailingNodeExtension"],
+  },
   {
-  "moduleName": "remirror/extension/underline",
-  "exports": [
-    "UnderlineExtension"
-  ]
-},
+    moduleName: "remirror/extension/underline",
+    exports: ["UnderlineExtension"],
+  },
   {
-  "moduleName": "remirror/preset/core",
-  "exports": [
-    "CorePreset"
-  ]
-},
+    moduleName: "remirror/preset/core",
+    exports: ["CorePreset"],
+  },
   {
-  "moduleName": "remirror/preset/embed",
-  "exports": [
-    "EmbedPreset",
-    "IframeExtension"
-  ]
-},
+    moduleName: "remirror/preset/embed",
+    exports: ["EmbedPreset", "IframeExtension"],
+  },
   {
-  "moduleName": "remirror/preset/list",
-  "exports": [
-    "BulletListExtension",
-    "ListItemExtension",
-    "ListPreset",
-    "OrderedListExtension"
-  ]
-},
+    moduleName: "remirror/preset/list",
+    exports: [
+      "BulletListExtension",
+      "ListItemExtension",
+      "ListPreset",
+      "OrderedListExtension",
+    ],
+  },
   {
-  "moduleName": "remirror/preset/react",
-  "exports": [
-    "ReactPreset"
-  ]
-},
+    moduleName: "remirror/preset/react",
+    exports: ["ReactPreset"],
+  },
   {
-  "moduleName": "remirror/preset/table",
-  "exports": [
-    "TableCellExtension",
-    "TableExtension",
-    "TableHeaderCell",
-    "TablePreset",
-    "TableRowExtension"
-  ]
-}
+    moduleName: "remirror/preset/table",
+    exports: [
+      "TableCellExtension",
+      "TableExtension",
+      "TableHeaderCell",
+      "TablePreset",
+      "TableRowExtension",
+    ],
+  },
 ];
