284790785dea7774a0110a047f6f33bb11836bd6
Benjie Gillam <benjie@jemjie.com>
2020-06-11 12:30:07 +0100

    Persisting state between checkboxes

--- @remirror/playground/src/use-remirror-playground.tsx
+++ @remirror/playground/src/use-remirror-playground.tsx
@@ -2,10 +2,39 @@ import { useEffect } from 'react';
 
 import { useRemirror } from '@remirror/react';
 
+declare global {
+  interface Window {
+    REMIRROR_PLAYGROUND_PERSIST: {
+      previousManager: any;
+      lastKnownGoodState: any;
+    };
+  }
+}
+
+if (!window.REMIRROR_PLAYGROUND_PERSIST) {
+  window.REMIRROR_PLAYGROUND_PERSIST = { previousManager: null, lastKnownGoodState: null };
+}
+
+const PERSIST = window.REMIRROR_PLAYGROUND_PERSIST;
+
+PERSIST.previousManager = null;
+PERSIST.lastKnownGoodState = null;
+
 export function useRemirrorPlayground() {
-  // Serialize/deserialize
   const remirror = useRemirror();
+  const manager = remirror.manager;
   useEffect(() => {
-    console.log(remirror.state.newState);
-  }, [remirror.state.newState]);
+    if (manager != PERSIST.previousManager) {
+      // The manager has changed - implies new code context
+
+      PERSIST.previousManager = manager;
+      if (PERSIST.lastKnownGoodState) {
+        // RESTORE STATE
+        remirror.setContent(PERSIST.lastKnownGoodState.doc);
+      }
+    } else {
+      // STORE STATE
+      PERSIST.lastKnownGoodState = remirror.state.newState;
+    }
+  }, [remirror.state.newState, manager]);
 }
