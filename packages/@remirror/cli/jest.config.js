3b81e8692e6397581ddd7ec6d39955123dcdbe4f
Ifiok Jr <ifiokotung@gmail.com>
2020-07-02 18:56:48 +0100

    chore: remove unused jest files

--- packages/@remirror/cli/jest.config.js
+++
@@ -1,7 +0,0 @@
-const config = require('../../../support/jest/jest.config');
-
-module.exports = {
-  ...config,
-  displayName: { name: require('./package.json').name.replace('@remirror/', ''), color: 'pink' },
-  testEnvironment: 'node',
-};
--- packages/jest-prosemirror/jest.config.js
+++
@@ -1,9 +0,0 @@
-const config = require('../../support/jest/jest.config');
-
-module.exports = {
-  ...config,
-  setupFilesAfterEnv: [],
-  snapshotSerializers: [],
-
-  displayName: { name: require('./package.json').name.replace('@remirror/', ''), color: 'pink' },
-};
