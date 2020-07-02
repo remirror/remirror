2f479a3201b7398e12f0bf759072c31c34040036
Ifiok Jr <ifiokotung@gmail.com>
2020-07-02 12:27:23 +0100

    fix: add babel to playground again

--- support/website/src/pages/playground.tsx
+++ support/website/src/pages/playground.tsx
@@ -1,11 +1,15 @@
 import Layout from '@theme/Layout';
 import React from 'react';
+import Head from '@docusaurus/Head';
 
 import { Playground } from '@remirror/playground';
 
 const PlaygroundPage = () => {
   return (
     <Layout>
+      <Head>
+        <script src='https://unpkg.com/@babel/standalone/babel.min.js'></script>
+      </Head>
       <Playground />
     </Layout>
   );
