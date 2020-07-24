9b132f2306aa20be20ce8e12c2eeccebcbfacc2c
Ifiok Jr <ifiokotung@gmail.com>
2020-07-24 03:49:52 +0100

    refactor(jest-remirror): remove `renderEditorString` (#375)
    Also upgrade dependencies.

--- packages/jest-remirror/src/index.ts
+++ packages/jest-remirror/src/index.ts
@@ -2,7 +2,6 @@ export * from './jest-remirror-builder';
 export * from './jsdom-polyfills';
 export * from './jest-remirror-matchers';
 export * from './jest-remirror-editor';
-export * from './jest-remirror-ssr';
 export * from './jest-remirror-environment';
 export * from './jest-remirror-utils';
 export * from './jest-remirror-types';
