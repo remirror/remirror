9d708c033a2007b1051cb24de79d1514020e0653
Ifiok Jr <ifiokotung@gmail.com>
2020-07-30 09:20:40 +0100

    fix(preset-social): reduce `AutoLinkExtension` priority (#410)
    - Remove priority override for the mention and emoji extensions

--- packages/@remirror/preset-social/src/__tests__/social-preset.spec.ts
+++ packages/@remirror/preset-social/src/__tests__/social-preset.spec.ts
@@ -1,7 +1,31 @@
+import { renderEditor } from 'jest-remirror';
+
+import { ExtensionPriority } from '@remirror/core';
+import { AutoLinkExtension } from '@remirror/extension-auto-link';
+import { EmojiExtension } from '@remirror/extension-emoji';
+import { MentionExtension } from '@remirror/extension-mention';
 import { isPresetValid } from '@remirror/testing';
 
 import { SocialPreset } from '..';
 
 test('is valid', () => {
   expect(isPresetValid(SocialPreset, { matchers: [] }));
+});
+
+test('can override extensions', () => {
+  const autoLinkExtension = new AutoLinkExtension({ defaultProtocol: 'https:' });
+  const mentionExtension = new MentionExtension({ matchers: [] });
+  const emojiExtension = new EmojiExtension({ priority: ExtensionPriority.Low });
+  const socialPreset = new SocialPreset();
+  const { manager } = renderEditor([
+    autoLinkExtension,
+    mentionExtension,
+    emojiExtension,
+    socialPreset,
+  ]);
+
+  expect(manager.getExtension(AutoLinkExtension)).toBe(autoLinkExtension);
+  expect(manager.getExtension(MentionExtension)).toBe(mentionExtension);
+  expect(manager.getExtension(EmojiExtension)).not.toBe(emojiExtension);
+  expect(manager.getExtension(EmojiExtension)).toBe(socialPreset.getExtension(EmojiExtension));
 });
