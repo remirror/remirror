fc5a5737a0332df8090cdb49fca314411c60d246
Ronny Roeller <ronny@collaborne.com>
2020-11-05 16:14:21 +0100

    Remove unnecessary import

--- support/storybook/stories/link-extension.stories.tsx
+++ support/storybook/stories/link-extension.stories.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useMemo } from 'react';
+import React, { FC, useEffect } from 'react';
 import { LinkExtension, LinkOptions } from 'remirror/extension/link';
 import { RemirrorProvider, useManager, useRemirror } from 'remirror/react';
 
