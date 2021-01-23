ff9a490946c547234cbc602a877a4444e77d9264
Ifiok Jr <ifiokotung@gmail.com>
2021-01-23 19:09:29 +0000

    feat(extension-font-family): new package 🎉

--- packages/remirror__extension-font-family/src/font-family-extension.ts
+++ packages/remirror__extension-font-family/src/font-family-extension.ts
@@ -1,13 +1,100 @@
-import { extension, PlainExtension } from '@remirror/core';
+import {
+  ApplySchemaAttributes,
+  command,
+  CommandFunction,
+  extension,
+  ExtensionPriority,
+  ExtensionTag,
+  isElementDomNode,
+  isString,
+  joinStyles,
+  MarkExtension,
+  MarkExtensionSpec,
+  MarkSpecOverride,
+  omitExtraAttributes,
+  ProsemirrorAttributes,
+} from '@remirror/core';
 
-export interface FontFamilyOptions {}
+const FONT_FAMILY_ATTRIBUTE = 'data-font-family';
+
+export type FontFamilyAttributes = ProsemirrorAttributes<{
+  fontFamily?: string;
+}>;
 
 /**
- * Add a font family to selected text, or text within the provided range.
+ * Formatting for text casing in your editor.
  */
-@extension<FontFamilyOptions>({})
-export class FontFamilyExtension extends PlainExtension<FontFamilyOptions> {
+@extension({})
+export class FontFamilyExtension extends MarkExtension {
   get name() {
     return 'fontFamily' as const;
+  }
+
+  createTags() {
+    return [ExtensionTag.FontStyle, ExtensionTag.FormattingMark];
+  }
+
+  createMarkSpec(extra: ApplySchemaAttributes, override: MarkSpecOverride): MarkExtensionSpec {
+    return {
+      ...override,
+      attrs: { ...extra.defaults(), fontFamily: { default: null } },
+      parseDOM: [
+        {
+          tag: `span[${FONT_FAMILY_ATTRIBUTE}]`,
+          getAttrs: (dom) => {
+            if (!isElementDomNode(dom)) {
+              return false;
+            }
+
+            const fontFamily = dom.getAttribute(FONT_FAMILY_ATTRIBUTE);
+
+            if (!fontFamily) {
+              return false;
+            }
+
+            return { ...extra.parse(dom), fontFamily };
+          },
+        },
+        {
+          // Get the color from the css style property. This is useful for pasted content.
+          style: 'text-transform',
+          priority: ExtensionPriority.Low,
+          getAttrs: (fontFamily) => {
+            if (!isString(fontFamily)) {
+              return false;
+            }
+
+            return {
+              fontFamily: fontFamily ? fontFamily.replace(/["']/g, '') : '',
+            };
+          },
+        },
+      ],
+      toDOM: (mark) => {
+        const { fontFamily } = omitExtraAttributes(mark.attrs, extra) as FontFamilyAttributes;
+        const extraAttrs = extra.dom(mark);
+        let style = extraAttrs.style;
+
+        style = joinStyles({ fontFamily }, style);
+
+        return ['span', { ...extraAttrs, style, [FONT_FAMILY_ATTRIBUTE]: fontFamily }, 0];
+      },
+    };
+  }
+
+  /**
+   * Set the font family for the selected text.
+   */
+  @command()
+  setFontFamily(fontFamily: string): CommandFunction {
+    return this.store.commands.applyMark.original(this.type, { fontFamily });
+  }
+}
+
+declare global {
+  namespace Remirror {
+    interface AllExtensions {
+      fontFamily: FontFamilyExtension;
+    }
   }
 }
