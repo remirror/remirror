0404118dabdc4b621634445c96d5aa2963c37c8b
ocavue <ocavue@gmail.com>
2021-02-25 23:27:12 +0800

    add more command examples

--- examples/storybook-react/src/table.stories.tsx
+++ examples/storybook-react/src/table.stories.tsx
@@ -5,27 +5,68 @@ import { Remirror, ThemeProvider, useRemirror, useRemirrorContext } from '@remir
 
 export default { title: 'Tables extension' };
 
-const Menu = () => {
+const CommandMenu = () => {
   const ctx = useRemirrorContext();
   const schema = ctx.schema;
   const customCellContent = schema.nodes.heading.create({ level: 3 }, [
     schema.text('hello', [schema.marks.textColor.create({ color: '#ffc0cb' })]),
     schema.text('world', [schema.marks.textColor.create({ color: '#87cefa' })]),
   ]);
-  const createTable = ctx.commands.createTable;
+  const commands = ctx.commands;
 
   return (
     <div>
-      <button onClick={() => createTable()}>insert a table with the default options</button>
-      <button onClick={() => createTable({ rowsCount: 4, columnsCount: 4, withHeaderRow: false })}>
-        insert a 4*4 table without headers
-      </button>
-      <button onClick={() => createTable({ rowsCount: 4, columnsCount: 4, withHeaderRow: true })}>
-        insert a 4*4 table with headers
-      </button>
-      <button onClick={() => createTable({ cellContent: customCellContent })}>
-        insert a table with custom cell content
-      </button>
+      <p>commands:</p>
+      <p
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyItems: 'flex-start',
+          alignItems: 'flex-start',
+        }}
+      >
+        <button onClick={() => commands.createTable()}>
+          create a table with the default options
+        </button>
+
+        <button
+          onClick={() =>
+            commands.createTable({ rowsCount: 4, columnsCount: 4, withHeaderRow: false })
+          }
+        >
+          create a 4*4 table without headers
+        </button>
+
+        <button
+          onClick={() =>
+            commands.createTable({ rowsCount: 4, columnsCount: 4, withHeaderRow: true })
+          }
+        >
+          create a 4*4 table with headers
+        </button>
+
+        <button onClick={() => commands.createTable({ cellContent: customCellContent })}>
+          create a table with custom cell content
+        </button>
+
+        <button onClick={() => commands.createTable({ cellContent: customCellContent })}>
+          create a table with custom cell content
+        </button>
+
+        <button onClick={() => commands.deleteTableColumn()}>delete the current column</button>
+
+        <button onClick={() => commands.deleteTableRow()}>delete the current row</button>
+
+        <button onClick={() => commands.addTableColumnAfter()}>
+          add a column after the current column
+        </button>
+
+        <button onClick={() => commands.addTableRowBefore()}>
+          add a column before the current row
+        </button>
+
+        <button onClick={() => commands.deleteTable()}>delete the table</button>
+      </p>
     </div>
   );
 };
@@ -35,8 +76,8 @@ const InnerEditor = () => {
 
   return (
     <div>
-      <Menu />
       <div {...getRootProps()} />
+      <CommandMenu />
     </div>
   );
 };
