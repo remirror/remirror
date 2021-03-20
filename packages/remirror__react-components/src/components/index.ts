770fe0b3416544779eccdb7ed182b252940f8fa4
Ronny Roeller <ronny@collaborne.com>
2021-03-20 07:59:46 +0100

    Expose EmojiPopupComponent to public
    The EmojiPopupComponent renders the emoji suggestion dropdown for the user. The component existed so far only internal. This commit exposes it to public.
    
    Discussed here: https://discord.com/channels/726035064831344711/745695521305526302/822569257090023424

--- packages/remirror__react-components/src/components/index.ts
+++ packages/remirror__react-components/src/components/index.ts
@@ -1,4 +1,5 @@
 export * from './color-picker-component';
 export * from './command-icon-component';
 export * from './dialog-component';
+export * from './emoji-popup-component';
 export * from './table-size-editor-component';
