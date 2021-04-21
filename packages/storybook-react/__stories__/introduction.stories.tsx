2639bd54381ab956ce295c5f3314077bc65029c5
ocavue <ocavue@gmail.com>
2021-04-21 20:35:08 +0800

    f

---
+++ packages/storybook-react/__stories__/introduction.stories.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const devInfo = [
+  // These environment variable will be available in GitHub Action workflows.
+  process.env.STORYBOOK_BUILD_TIME ?? 'unknown',
+  (process.env.STORYBOOK_GIT_SHA ?? 'unknown').slice(0, 7),
+  process.env.STORYBOOK_GIT_REF ?? 'unknown',
+  process.env.STORYBOOK_MODE ?? 'development',
+].join(' · ');
+
+export const Introduction: React.FC = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        fontFamily: 'sans-serif',
+        padding: '16px',
+      }}
+    >
+      <h1>Welcome to remirror storybook.</h1>
+      <div style={{ height: '64px' }} />
+      <p>
+        <div>{devInfo}</div>
+      </p>
+    </div>
+  );
+};
+
+export default {
+  component: Introduction,
+  title: 'Introduction',
+  parameters: {
+    layout: 'centered',
+  },
+};
