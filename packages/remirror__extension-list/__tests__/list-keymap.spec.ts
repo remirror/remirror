7cf404814c03131688b79c948c3cd8a1e3edf1a6
ocavue <ocavue@gmail.com>
2021-06-21 14:14:38 +0800

    add tests

--- packages/remirror__extension-list/__tests__/list-keymap.spec.ts
+++ packages/remirror__extension-list/__tests__/list-keymap.spec.ts
@@ -85,3 +85,205 @@ describe('Enter', () => {
     expect(editor.view.state.doc).toEqualProsemirrorNode(to);
   });
 });
+
+describe('Tab and Shift-Tab', () => {
+  const editor = renderEditor([
+    new TaskListExtension({}),
+    new BulletListExtension({}),
+    new OrderedListExtension(),
+    new TaskListExtension(),
+  ]);
+  const {
+    nodes: { doc, paragraph: p, orderedList: ol, bulletList: ul, listItem: li, taskList: tl },
+    attributeNodes: { taskListItem },
+  } = editor;
+  const unchecked = taskListItem({ checked: false });
+  const checked = taskListItem({ checked: true });
+
+  let from: TaggedProsemirrorNode, to: TaggedProsemirrorNode;
+
+  it('sink a list item', () => {
+    from = doc(
+      ul(
+        li(p('hello')),
+        li(p('world<cursor>')), //
+      ),
+    );
+    to = doc(
+      ul(
+        li(
+          p('hello'),
+          ul(
+            li(p('world')), //
+          ),
+        ),
+      ),
+    );
+    editor.add(from).press('Tab');
+    expect(editor.view.state.doc).toEqualProsemirrorNode(to);
+  });
+
+  it('lift a list item', () => {
+    from = doc(
+      ul(
+        li(
+          p('hello'),
+          ul(
+            li(p('worl<cursor>d')), //
+          ),
+        ),
+      ),
+    );
+    to = doc(
+      ul(
+        li(p('hello')),
+        li(p('world')), //
+      ),
+    );
+    editor.add(from).press('Shift-Tab');
+    expect(editor.view.state.doc).toEqualProsemirrorNode(to);
+  });
+
+  it('sink and lift the correct list item in a nested and mixed list', () => {
+    from = doc(
+      ul(
+        li(
+          p('hello'),
+          tl(
+            checked(
+              p('hello'), //
+            ),
+            unchecked(
+              p('world'), //
+              ol(
+                li(p('hello')), //
+                li(p('world<cursor>')), //
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+    to = doc(
+      ul(
+        li(
+          p('hello'),
+          tl(
+            checked(
+              p('hello'), //
+            ),
+            unchecked(
+              p('world'), //
+              ol(
+                li(
+                  p('hello'),
+                  ol(
+                    li(p('world')), //
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+    editor.add(from).press('Tab');
+    expect(editor.view.state.doc).toEqualProsemirrorNode(to);
+    editor.press('Shift-Tab');
+    expect(editor.view.state.doc).toEqualProsemirrorNode(from);
+
+    from = doc(
+      ol(
+        li(
+          p('hello'),
+          ul(
+            li(
+              p('hello'), //
+            ),
+            li(
+              p('world'), //
+              tl(
+                unchecked(p('hello')), //
+                checked(p('world<cursor>')), //
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+    to = doc(
+      ol(
+        li(
+          p('hello'),
+          ul(
+            li(
+              p('hello'), //
+            ),
+            li(
+              p('world'), //
+              tl(
+                unchecked(
+                  p('hello'),
+                  tl(
+                    checked(p('world')), //
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+    editor.add(from).press('Tab');
+    expect(editor.view.state.doc).toEqualProsemirrorNode(to);
+    editor.press('Shift-Tab');
+    expect(editor.view.state.doc).toEqualProsemirrorNode(from);
+
+    from = doc(
+      tl(
+        checked(
+          p('hello'),
+          ol(
+            li(
+              p('hello'), //
+            ),
+            li(
+              p('world'), //
+              ol(
+                li(p('hello')), //
+                li(p('world<cursor>')), //
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+    to = doc(
+      tl(
+        checked(
+          p('hello'),
+          ol(
+            li(
+              p('hello'), //
+            ),
+            li(
+              p('world'), //
+              ol(
+                li(
+                  p('hello'),
+                  ol(
+                    li(p('world')), //
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+    editor.add(from).press('Tab');
+    expect(editor.view.state.doc).toEqualProsemirrorNode(to);
+    editor.press('Shift-Tab');
+    expect(editor.view.state.doc).toEqualProsemirrorNode(from);
+  });
+});
