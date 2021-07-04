33e8a0ed58f94e2bb9c0a73a0915ed2aae6470e6
ocavue <ocavue@gmail.com>
2021-07-04 20:08:43 +0800

    add tests

--- packages/remirror__extension-list/__tests__/list-input-rules.spec.ts
+++ packages/remirror__extension-list/__tests__/list-input-rules.spec.ts
@@ -92,4 +92,210 @@ describe('create a list', () => {
       ),
     );
   });
+
+  it('creates a taskList in a multi-items list', () => {
+    editor.add(
+      doc(
+        bulletList(
+          listItem(p('123')),
+          listItem(p('456')),
+          listItem(p('<cursor>')), //
+        ),
+      ),
+    );
+    editor.insertText('[x] ');
+    expect(editor.doc).toEqualProsemirrorNode(
+      doc(
+        bulletList(
+          listItem(p('123')), //
+          listItem(p('456')), //
+        ),
+        taskList(
+          checkedItem(p('')), //
+        ),
+      ),
+    );
+
+    editor.add(
+      doc(
+        bulletList(
+          listItem(p('123')),
+          listItem(p('<cursor>')), //
+          listItem(p('789')),
+        ),
+      ),
+    );
+    editor.insertText('[x] ');
+    expect(editor.doc).toEqualProsemirrorNode(
+      doc(
+        bulletList(
+          listItem(p('123')), //
+        ),
+        taskList(
+          checkedItem(p('')), //
+        ),
+        bulletList(
+          listItem(p('789')), //
+        ),
+      ),
+    );
+
+    editor.add(
+      doc(
+        bulletList(
+          listItem(p('<cursor>')), //
+          listItem(p('456')),
+          listItem(p('789')),
+        ),
+      ),
+    );
+    editor.insertText('[x] ');
+    expect(editor.doc).toEqualProsemirrorNode(
+      doc(
+        taskList(
+          checkedItem(p('')), //
+        ),
+        bulletList(
+          listItem(p('456')), //
+          listItem(p('789')), //
+        ),
+      ),
+    );
+  });
+
+  it('creates a taskList in a nested list', () => {
+    editor.add(
+      doc(
+        bulletList(
+          listItem(
+            p('123'),
+            bulletList(
+              listItem(p('<cursor>')), //
+            ),
+          ),
+        ),
+      ),
+    );
+    editor.insertText('[x] ');
+    expect(editor.doc).toEqualProsemirrorNode(
+      doc(
+        bulletList(
+          listItem(
+            p('123'),
+            taskList(
+              checkedItem(p('<cursor>')), //
+            ),
+          ),
+        ),
+      ),
+    );
+
+    editor.add(
+      doc(
+        bulletList(
+          listItem(
+            p('123'),
+            bulletList(
+              listItem(p('abc')), //
+              listItem(p('<cursor>')), //
+            ),
+          ),
+        ),
+      ),
+    );
+    editor.insertText('[x] ');
+    expect(editor.doc).toEqualProsemirrorNode(
+      doc(
+        bulletList(
+          listItem(
+            p('123'),
+            bulletList(
+              listItem(p('abc')), //
+            ),
+            taskList(
+              checkedItem(p('<cursor>')), //
+            ),
+          ),
+        ),
+      ),
+    );
+
+    editor.add(
+      doc(
+        bulletList(
+          listItem(
+            p('123'),
+            bulletList(
+              listItem(p('<cursor>')), //
+              listItem(p('def')), //
+            ),
+          ),
+        ),
+      ),
+    );
+    editor.insertText('[x] ');
+    expect(editor.doc).toEqualProsemirrorNode(
+      doc(
+        bulletList(
+          listItem(
+            p('123'),
+            taskList(
+              checkedItem(p('<cursor>')), //
+            ),
+            bulletList(
+              listItem(p('def')), //
+            ),
+          ),
+        ),
+      ),
+    );
+
+    editor.add(
+      doc(
+        bulletList(
+          listItem(
+            p('123'),
+            bulletList(
+              listItem(p('abc')), //
+              listItem(p('<cursor>')), //
+              listItem(p('def')), //
+            ),
+          ),
+        ),
+      ),
+    );
+    editor.insertText('[ ] ');
+    expect(editor.doc).toEqualProsemirrorNode(
+      doc(
+        bulletList(
+          listItem(
+            p('123'),
+            bulletList(
+              listItem(p('abc')), //
+            ),
+            taskList(
+              uncheckedItem(p('<cursor>')), //
+            ),
+            bulletList(
+              listItem(p('def')), //
+            ),
+          ),
+        ),
+      ),
+    );
+  });
+
+  it('does not create a list if already inside a such list', () => {
+    editor.add(doc(bulletList(listItem(p('<cursor>')))));
+    editor.insertText('- ');
+    expect(editor.doc).toEqualProsemirrorNode(doc(bulletList(listItem(p('- ')))));
+
+    editor.add(doc(orderedList({ order: 1 })(listItem(p('<cursor>')))));
+    editor.insertText('1. ');
+    expect(editor.doc).toEqualProsemirrorNode(doc(orderedList({ order: 1 })(listItem(p('1. ')))));
+
+    editor.add(doc(taskList(uncheckedItem(p('<cursor>')))));
+    editor.insertText('[x] ');
+    expect(editor.doc).toEqualProsemirrorNode(doc(taskList(uncheckedItem(p('[x] ')))));
+  });
 });
