70e33f64ccd3fc41c2963af01f69a922a3394736
Ifiok Jr <ifiokotung@gmail.com>
2021-07-18 07:22:31 +0100

    chore: remove examples folder

--- examples/with-next/__e2e__/wysiwyg-editor-ssr.test.ts
+++
@@ -1,7 +0,0 @@
-import { smokeTest, ssrTest } from 'testing/playwright';
-
-const path = 'editor/wysiwyg';
-
-smokeTest(path);
-ssrTest('wysiwyg without content', path);
-ssrTest('wysiwyg with content', 'editor/wysiwyg/content');
--- examples/with-react-native/src/webview/babel.config.js
+++
@@ -1,6 +0,0 @@
-const config = require('../../../../support/base.babel');
-
-module.exports = {
-  ...config,
-  presets: [...config.presets, '@linaria'],
-};
