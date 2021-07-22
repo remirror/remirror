3cc504977e5cb3744a464cc12763c00b09b4d023
ocavue <ocavue@gmail.com>
2021-07-23 01:08:25 +0800

    fix(extension-list): fix CJK IME in the list

--- packages/remirror__extension-list/src/list-item-node-view.ts
+++ packages/remirror__extension-list/src/list-item-node-view.ts
@@ -1,14 +1,16 @@
-import type { NodeView } from '@remirror/pm/view';
+import type { EditorView, NodeView } from '@remirror/pm/view';
 import { ExtensionListTheme } from '@remirror/theme';
 
 export function createCustomMarkListItemNodeView({
   mark,
   extraAttrs,
   extraClasses,
+  view,
 }: {
   mark: HTMLElement;
   extraAttrs?: Record<string, string>;
   extraClasses?: string[];
+  view: EditorView;
 }): NodeView {
   const dom = document.createElement('li');
   dom.classList.add(ExtensionListTheme.LIST_ITEM_WITH_CUSTOM_MARKER);
@@ -30,7 +32,13 @@ export function createCustomMarkListItemNodeView({
   // When a list item node's `content` updates, it's necessary to re-run the
   // nodeView function so that the list item node's `disabled` class can be
   // updated.
+  //
+  // However, when users are using IME, never re-create the nodeView. See also #1017.
   const update = (): boolean => {
+    if (view.composing) {
+      return true;
+    }
+
     return false;
   };
 
