2bd63a39320d6c795052e427c5abcbc3cc15492e
ocavue <ocavue@gmail.com>
2021-08-04 11:56:36 +0800

    fix INSERT_EMOJI_LABEL

--- packages/remirror__messages/src/extension-emoji-messages.ts
+++ packages/remirror__messages/src/extension-emoji-messages.ts
@@ -2,7 +2,7 @@ import type { MessageDescriptor } from '@lingui/core';
 import { defineMessage } from '@lingui/macro';
 
 export const INSERT_EMOJI_LABEL: MessageDescriptor = defineMessage({
-  id: 'extension.command.toggle-bold.label',
+  id: 'extension.command.insert-emoji.label',
   comment: 'Label for inserting an emoji.',
   message: 'Insert Emoji',
 });
