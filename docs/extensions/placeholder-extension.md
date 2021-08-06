ef91e890821fa470f1828be19f46cd123303bdd0
Ronny Roeller <ronnyroeller@users.noreply.github.com>
2021-08-06 14:21:55 +0200

    docs: add placeholder extension (#1038)

---
+++ docs/extensions/placeholder-extension.md
@@ -0,0 +1,44 @@
+---
+hide_title: true
+title: 'PlaceholderExtension'
+---
+
+# `PlaceholderExtension`
+
+## Summary
+
+This extension shows a placeholder when the **ProseMirror** content of your editor is empty.
+
+## Features
+
+### Custom styling
+
+The placeholder can be styled with a custom CSS class.
+
+## Usage
+
+### Installation
+
+This extension is installed for you when you install the main `remirror` package.
+
+You can use the imports in the following way.
+
+```ts
+import { placeholderExtension } from 'remirror/extensions';
+```
+
+To install it directly you can use
+
+The extension is provided by the `@remirror/extension-placeholder` package. There are two ways of pulling it into your project.
+
+### Examples
+
+See [storybook](https://remirror.vercel.app/?path=/story/placeholder-extension--basic) for examples.
+
+## API
+
+### Options
+
+### Commands
+
+### Helpers
---
+++ packages/remirror__extension-placeholder/__stories__/placeholder-extension.stories.tsx
@@ -0,0 +1,40 @@
+import { useCallback } from 'react';
+import { PlaceholderExtension } from 'remirror/extensions';
+import { Remirror, ThemeProvider, useRemirror } from '@remirror/react';
+
+import './styles.css';
+
+export default { title: 'Placeholder Extension' };
+
+export const Basic = (): JSX.Element => {
+  const extensions = useCallback(
+    () => [new PlaceholderExtension({ placeholder: `I'm a placeholder!` })],
+    [],
+  );
+  const { manager } = useRemirror({ extensions });
+
+  return (
+    <ThemeProvider>
+      <Remirror manager={manager} />
+    </ThemeProvider>
+  );
+};
+
+export const CustomStyle = (): JSX.Element => {
+  const extensions = useCallback(
+    () => [
+      new PlaceholderExtension({
+        placeholder: `I'm a styled placeholder!`,
+        emptyNodeClass: 'my-placeholder',
+      }),
+    ],
+    [],
+  );
+  const { manager } = useRemirror({ extensions });
+
+  return (
+    <ThemeProvider>
+      <Remirror manager={manager} />
+    </ThemeProvider>
+  );
+};
