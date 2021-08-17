3c68e61a13ffdf1f109dca333c4b0ab048afb682
Ronny Roeller <ronnyroeller@users.noreply.github.com>
2021-08-17 12:25:17 +0200

    docs: add storybook for react-renderer (static HTML) (#1064)

---
+++ packages/remirror__react-renderer/__stories__/react-renderer.stories.tsx
@@ -0,0 +1,49 @@
+import {
+  Callout,
+  CodeBlock,
+  createIFrameHandler,
+  createLinkHandler,
+  Doc,
+  Heading,
+  MarkMap,
+  RemirrorRenderer,
+  TextHandler,
+  ThemeProvider,
+} from '@remirror/react';
+
+import { SAMPLE_DOC } from './sample-doc';
+
+export default { title: 'React Renderer (static HTML)' };
+
+const typeMap: MarkMap = {
+  blockquote: 'blockquote',
+  bulletList: 'ul',
+  callout: Callout,
+  codeBlock: CodeBlock,
+  doc: Doc,
+  hardBreak: 'br',
+  heading: Heading,
+  horizontalRule: 'hr',
+  iframe: createIFrameHandler(),
+  image: 'img',
+  listItem: 'li',
+  paragraph: 'p',
+  orderedList: 'ol',
+  text: TextHandler,
+};
+
+const markMap: MarkMap = {
+  italic: 'em',
+  bold: 'strong',
+  code: 'code',
+  link: createLinkHandler({ target: '_blank' }),
+  underline: 'u',
+};
+
+export const Basic = (): JSX.Element => {
+  return (
+    <ThemeProvider>
+      <RemirrorRenderer json={SAMPLE_DOC} typeMap={typeMap} markMap={markMap} />
+    </ThemeProvider>
+  );
+};
