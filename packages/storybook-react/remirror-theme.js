f30557125296fb1ba687a2f7ba38680949cd7e77
Ronny Roeller <ronnyroeller@users.noreply.github.com>
2021-08-17 15:34:25 +0200

    docs: improve storybook (#1066)
    Show source code with stories
    Align storybook design with remirror design

---
+++ packages/storybook-react/remirror-theme.js
@@ -0,0 +1,11 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Remirror',
+  brandUrl: 'https://www.remirror.io',
+  brandImage: 'https://remirror.io/img/logo.svg',
+  appBg: '#7963d222',
+  barTextColor: '#7963d2ee',
+  barSelectedColor: '#7963d2',
+});
