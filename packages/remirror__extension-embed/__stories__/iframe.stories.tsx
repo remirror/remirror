e082c0ba1414baf8ab146c37f83d096f5fcadeed
Ronny Roeller <ronnyroeller@users.noreply.github.com>
2021-08-18 13:54:42 +0200

    docs: explain usage of EmbedExtension (#1075)

--- packages/remirror__extension-embed/__stories__/iframe.stories.tsx
+++ packages/remirror__extension-embed/__stories__/iframe.stories.tsx
@@ -1,70 +1,62 @@
 import 'remirror/styles/all.css';
 
-import React, { useState } from 'react';
+import React from 'react';
 import { IframeExtension } from 'remirror/extensions';
-import { htmlToProsemirrorNode } from '@remirror/core';
 import { Remirror, ThemeProvider, useCommands, useRemirror } from '@remirror/react';
 
 export default { title: 'Extensions / Embed' };
 
+const AddIframeButton = () => {
+  const commands = useCommands();
+  const handleClick = () =>
+    commands.addIframe({ src: 'https://remirror.io/', height: 250, width: 500 });
+  return <button onClick={handleClick}>Add iframe</button>;
+};
+
 export const Basic: React.FC = () => {
-  const { manager, state, onChange } = useRemirror({
+  const { manager } = useRemirror({
     extensions: () => [new IframeExtension()],
-    content: '<p>click to add a fixed-size iframe</p>',
-    stringHandler: htmlToProsemirrorNode,
   });
 
   return (
     <ThemeProvider>
-      <Remirror manager={manager} autoFocus onChange={onChange} state={state} autoRender='end'>
-        <AddIframe />
+      <Remirror manager={manager} autoRender='end'>
+        <AddIframeButton />
       </Remirror>
     </ThemeProvider>
-  );
-};
-
-const AddIframe = () => {
-  const commands = useCommands();
-  const [src, setSrc] = useState('https://remirror.io/');
-  const handleClick = () => commands.addIframe({ src, height: 250, width: 500 });
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSrc(e.target.value);
-  };
-  return (
-    <>
-      <button onClick={handleClick}> Add iframe</button>
-      <input type='text' value={src} onChange={handleChange} />
-    </>
   );
 };
 
 export const Resizable: React.FC = () => {
-  const { manager, state, onChange } = useRemirror({
+  const { manager } = useRemirror({
     extensions: () => [new IframeExtension({ enableResizing: true })],
-    content: '<p>click to add a resizable iframe</p>',
-    stringHandler: htmlToProsemirrorNode,
   });
 
   return (
     <ThemeProvider>
-      <Remirror manager={manager} autoFocus onChange={onChange} state={state} autoRender='end'>
-        <AddResizableIframe />
+      <Remirror manager={manager} autoRender='end'>
+        <AddIframeButton />
       </Remirror>
     </ThemeProvider>
   );
 };
 
-const AddResizableIframe = () => {
+const AddYoutubeButton = () => {
   const commands = useCommands();
-  const [src, setSrc] = useState('https://remirror.io/');
-  const handleClick = () => commands.addIframe({ src, height: 250, width: 500 });
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSrc(e.target.value);
-  };
+  const handleClick = () => commands.addYouTubeVideo({ video: 'Zi7sRMcJT-o', startAt: 450 });
+  return <button onClick={handleClick}>Add video</button>;
+};
+
+export const Youtube: React.FC = () => {
+  const { manager } = useRemirror({
+    extensions: () => [new IframeExtension({ enableResizing: true })],
+  });
+
   return (
-    <>
-      <button onClick={handleClick}> Add resizable iframe</button>
-      <input type='text' value={src} onChange={handleChange} />
-    </>
+    <ThemeProvider>
+      <Remirror manager={manager} autoRender='end'>
+        <AddYoutubeButton />
+      </Remirror>
+    </ThemeProvider>
   );
 };
