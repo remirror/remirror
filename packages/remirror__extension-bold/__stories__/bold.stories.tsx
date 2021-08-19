453a2d1115768dd8ed74d75effa454c071e8928f
Ronny Roeller <ronnyroeller@users.noreply.github.com>
2021-08-19 17:37:46 +0200

    docs: explain how to use basic extensions (#1101)
    Docs for bold, italic, strike, and underline extension

---
+++ packages/remirror__extension-bold/__stories__/bold.stories.tsx
@@ -0,0 +1,38 @@
+import 'remirror/styles/all.css';
+import './styles.css';
+
+import { BoldExtension } from 'remirror/extensions';
+import { cx, htmlToProsemirrorNode } from '@remirror/core';
+import { ProsemirrorDevTools } from '@remirror/dev';
+import { Remirror, ThemeProvider, useActive, useCommands, useRemirror } from '@remirror/react';
+
+export default { title: 'Extensions / Bold' };
+
+const extensions = () => [new BoldExtension()];
+
+const BoldButton = () => {
+  const commands = useCommands();
+  const active = useActive(true);
+  return (
+    <button onClick={() => commands.toggleBold()} className={cx(active.bold() && 'active')}>
+      Bold
+    </button>
+  );
+};
+
+export const Basic = (): JSX.Element => {
+  const { manager, state, onChange } = useRemirror({
+    extensions: extensions,
+    content: '<p>Text in <b>bold</b></p>',
+    stringHandler: htmlToProsemirrorNode,
+  });
+
+  return (
+    <ThemeProvider>
+      <Remirror manager={manager} autoFocus onChange={onChange} state={state} autoRender='end'>
+        <BoldButton />
+        <ProsemirrorDevTools />
+      </Remirror>
+    </ThemeProvider>
+  );
+};
