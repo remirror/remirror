19db39363d64ebb8fbf0e381c2461c1bd6a8e96d
Ronny Roeller <ronny@collaborne.com>
2021-08-21 15:41:40 +0200

    docs: describe how to replace editor content

--- packages/remirror__react/__stories__/controlled.stories.tsx
+++ packages/remirror__react/__stories__/controlled.stories.tsx
@@ -1,15 +1,30 @@
-import { useState } from 'react';
-import { BoldExtension, ItalicExtension, UnderlineExtension } from 'remirror/extensions';
-import { Remirror, ThemeProvider, useRemirror } from '@remirror/react';
+import { useCallback, useState } from 'react';
+import { RemirrorJSON } from '@remirror/core';
+import { Remirror, ThemeProvider, useRemirror, useRemirrorContext } from '@remirror/react';
+
+const DOC: RemirrorJSON = {
+  type: 'doc',
+  content: [
+    {
+      type: 'paragraph',
+      content: [
+        {
+          type: 'text',
+          text: 'New content',
+        },
+      ],
+    },
+  ],
+};
 
 export default { title: 'Controlled Editor' };
 
-const extensions = () => [new BoldExtension({}), new ItalicExtension(), new UnderlineExtension()];
+const extensions = () => [];
 
-export const Basic = (): JSX.Element => {
+export const Editable = (): JSX.Element => {
   const { manager, state, onChange } = useRemirror({
     extensions,
-    content: '<p><u>Hello</u> there <b>friend</b> and <em>partner</em>.</p>',
+    content: '<p>Some text</p>',
     stringHandler: 'html',
   });
 
@@ -21,6 +36,48 @@ export const Basic = (): JSX.Element => {
       <button onClick={() => setEditable(!editable)}>
         Toggle editable (currently {editable.toString()})
       </button>
+    </ThemeProvider>
+  );
+};
+
+function SetContentButton() {
+  const { setContent } = useRemirrorContext();
+  return <button onClick={() => setContent(DOC)}>Replace content</button>;
+}
+
+export const ReplaceContentPreserveHistory = (): JSX.Element => {
+  const { manager, state, onChange } = useRemirror({
+    extensions,
+    content: '<p>Original content</p>',
+    stringHandler: 'html',
+  });
+
+  return (
+    <ThemeProvider>
+      <Remirror manager={manager} state={state} onChange={onChange} autoRender='end'>
+        <SetContentButton />
+      </Remirror>
+    </ThemeProvider>
+  );
+};
+
+export const ReplaceContentAndClearHistory = (): JSX.Element => {
+  const { manager, state, onChange } = useRemirror({
+    extensions,
+    content: '<p>Original content</p>',
+    stringHandler: 'html',
+  });
+
+  const handleClick = useCallback(() => {
+    // Clear out old state when setting data from outside
+    // This prevents e.g. the user from using CTRL-Z to go back to the old state
+    manager.view.updateState(manager.createState({ content: DOC }));
+  }, [manager]);
+
+  return (
+    <ThemeProvider>
+      <Remirror manager={manager} state={state} onChange={onChange} />
+      <button onClick={handleClick}>Replace content</button>
     </ThemeProvider>
   );
 };
