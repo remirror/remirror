15994364c524ef117c170068aeeb5660c2504134
Ronny Roeller <ronny@collaborne.com>
2021-08-23 15:24:22 +0200

    Document font family

---
+++ packages/remirror__extension-font-family/__stories__/font-family.stories.tsx
@@ -0,0 +1,38 @@
+import 'remirror/styles/all.css';
+
+import { FontFamilyExtension } from 'remirror/extensions';
+import { htmlToProsemirrorNode } from '@remirror/core';
+import { ProsemirrorDevTools } from '@remirror/dev';
+import { Remirror, ThemeProvider, useCommands, useRemirror } from '@remirror/react';
+
+export default { title: 'Extensions / Font family' };
+
+const extensions = () => [new FontFamilyExtension()];
+
+const FontFamilyButtons = () => {
+  const commands = useCommands();
+  return (
+    <>
+      <button onClick={() => commands.setFontFamily('serif')}>Serif</button>
+      <button onClick={() => commands.setFontFamily('sans-serif')}>Sans serif</button>
+    </>
+  );
+};
+
+export const Basic = (): JSX.Element => {
+  const { manager, state, onChange } = useRemirror({
+    extensions: extensions,
+    content:
+      '<p>Some text in <span style=" font-family:serif" data-font-family="serif">serif</span></p>',
+    stringHandler: htmlToProsemirrorNode,
+  });
+
+  return (
+    <ThemeProvider>
+      <Remirror manager={manager} autoFocus onChange={onChange} state={state} autoRender='end'>
+        <FontFamilyButtons />
+        <ProsemirrorDevTools />
+      </Remirror>
+    </ThemeProvider>
+  );
+};
