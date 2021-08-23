416f00f26703cf2dfae64344fc18c481c48940f0
ocavue <ocavue@users.noreply.github.com>
2021-08-23 20:35:38 +0000

    remove react-debugger

--- packages/remirror__react-debugger/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './list';
-export * from './node-picker';
-export * from './styled';
-export * from './tabs';
--- packages/remirror__react-debugger/src/components/tabs/json-editor-tab.tsx
+++
@@ -1,7 +0,0 @@
-/**
- * @module
- *
- * This module allows for direct editing of the content within the editor.
- */
-
-export {};
