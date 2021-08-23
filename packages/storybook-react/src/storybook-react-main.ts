680cfeaf8346a04b29d16920e95e2134432a8ba8
ocavue <ocavue@gmail.com>
2021-08-24 02:32:35 +0800

    move

--- packages/storybook-react/src/storybook-react-main.ts
+++ packages/storybook-react/src/storybook-react-main.ts
@@ -7,7 +7,7 @@ import { isArray, isPlainObject } from '@remirror/core';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const babelConfig = require(baseDir('packages', 'storybook-react', '.babelrc.js'));
 
-export const stories = glob.sync(baseDir('packages/*/__stories__/*.stories.(tsx)'), {
+export const stories = glob.sync(baseDir('packages/*/__stories__/**/*.stories.(tsx)'), {
   ignore: ['**/node_modules'],
 });
 
