b6ab5385bc2170d548c8a682f618199dfe470399
Ronny Roeller <ronnyroeller@users.noreply.github.com>
2021-08-25 13:48:41 +0200

    fix: bring back the social editor (#1156)

--- packages/storybook-react/stories/react-editors/social-editor.stories.tsx
+++ packages/storybook-react/stories/react-editors/social-editor.stories.tsx
@@ -1,7 +1,22 @@
+import { ProsemirrorDevTools } from '@remirror/dev';
 import { SocialEditor } from '@remirror/react-editors/social';
 
 export default { title: 'Editors / Social' };
 
+const ALL_USERS = [
+  { id: 'joe', label: 'Joe' },
+  { id: 'sue', label: 'Sue' },
+  { id: 'pat', label: 'Pat' },
+  { id: 'tom', label: 'Tom' },
+  { id: 'jim', label: 'Jim' },
+];
+
+const TAGS = ['editor', 'remirror', 'opensource', 'prosemirror'];
+
 export const Basic = () => {
-  return <SocialEditor />;
+  return (
+    <SocialEditor placeholder='Mention @joe or add #remirror' users={ALL_USERS} tags={TAGS}>
+      <ProsemirrorDevTools />
+    </SocialEditor>
+  );
 };
