28bf062cf1c4ab08f80ac3612c9544942c9a44f9
Ronny Roeller <ronny@collaborne.com>
2021-08-25 10:48:09 +0200

    fix: bring back the social editor

--- packages/remirror__react-editors/src/social/social-editor.tsx
+++ packages/remirror__react-editors/src/social/social-editor.tsx
@@ -1,35 +1,111 @@
-import { FC } from 'react';
+import { FC, useCallback, useMemo, useState } from 'react';
 import { IdentifierSchemaAttributes } from 'remirror';
-import { EmojiExtension, LinkExtension, MentionExtension } from 'remirror/extensions';
+import {
+  EmojiExtension,
+  LinkExtension,
+  MentionAtomExtension,
+  MentionAtomNodeAttributes,
+  PlaceholderExtension,
+  wysiwygPreset,
+} from 'remirror/extensions';
 import data from 'svgmoji/emoji.json';
 import {
+  ComponentItem,
   EditorComponent,
   EmojiPopupComponent,
+  FloatingToolbar,
+  MentionAtomPopupComponent,
+  MentionAtomState,
   Remirror,
   ThemeProvider,
+  ToolbarItemUnion,
   useRemirror,
 } from '@remirror/react';
 import { AllStyledComponent } from '@remirror/styles/emotion';
-
-const extensions = () => [
-  new LinkExtension({ autoLink: true }),
-  new MentionExtension({
-    matchers: [
-      { name: 'at', char: '@', appendText: ' ' },
-      { name: 'tag', char: '#', appendText: ' ' },
-    ],
-  }),
-  new EmojiExtension({ plainText: false, data, moji: 'noto' }),
-];
 
 const extraAttributes: IdentifierSchemaAttributes[] = [
   { identifiers: ['mention', 'emoji'], attributes: { role: { default: 'presentation' } } },
   { identifiers: ['mention'], attributes: { href: { default: null } } },
 ];
 
-export interface SocialEditorProps {}
+export interface SocialEditorProps extends Pick<MentionComponentProps, 'users' | 'tags'> {
+  placeholder?: string;
+}
 
-export const SocialEditor: FC = (props) => {
+const floatingToolbarItems: ToolbarItemUnion[] = [
+  {
+    type: ComponentItem.ToolbarGroup,
+    label: 'Simple Formatting',
+    items: [
+      {
+        type: ComponentItem.ToolbarCommandButton,
+        commandName: 'toggleHeading',
+        display: 'icon',
+        attrs: { level: 1 },
+      },
+      {
+        type: ComponentItem.ToolbarCommandButton,
+        commandName: 'toggleHeading',
+        display: 'icon',
+        attrs: { level: 2 },
+      },
+      { type: ComponentItem.ToolbarCommandButton, commandName: 'toggleBold', display: 'icon' },
+      { type: ComponentItem.ToolbarCommandButton, commandName: 'toggleItalic', display: 'icon' },
+      { type: ComponentItem.ToolbarCommandButton, commandName: 'toggleUnderline', display: 'icon' },
+      { type: ComponentItem.ToolbarCommandButton, commandName: 'toggleStrike', display: 'icon' },
+      { type: ComponentItem.ToolbarCommandButton, commandName: 'toggleCode', display: 'icon' },
+    ],
+  },
+];
+
+interface MentionComponentProps<
+  UserData extends MentionAtomNodeAttributes = MentionAtomNodeAttributes,
+> {
+  users?: UserData[];
+  tags?: string[];
+}
+
+function MentionComponent({ users, tags }: MentionComponentProps) {
+  const [mentionState, setMentionState] = useState<MentionAtomState | null>();
+  const tagItems = useMemo(
+    () => (tags ?? []).map((tag) => ({ id: tag, label: `#${tag}` })),
+    [tags],
+  );
+  const items = useMemo(() => {
+    if (!mentionState) {
+      return [];
+    }
+
+    const allItems = mentionState.name === 'at' ? users : tagItems;
+
+    if (!allItems) {
+      return [];
+    }
+
+    const query = mentionState.query.full.toLowerCase() ?? '';
+    return allItems.filter((item) => item.label.toLowerCase().includes(query)).sort();
+  }, [mentionState, users, tagItems]);
+
+  return <MentionAtomPopupComponent onChange={setMentionState} items={items} />;
+}
+
+export const SocialEditor: FC<SocialEditorProps> = ({ placeholder, ...props }) => {
+  const extensions = useCallback(
+    () => [
+      new PlaceholderExtension({ placeholder }),
+      new LinkExtension({ autoLink: true }),
+      new MentionAtomExtension({
+        matchers: [
+          { name: 'at', char: '@', appendText: ' ' },
+          { name: 'tag', char: '#', appendText: ' ' },
+        ],
+      }),
+      new EmojiExtension({ plainText: false, data, moji: 'noto' }),
+      ...wysiwygPreset(),
+    ],
+    [placeholder],
+  );
+
   const { children } = props;
   const { manager } = useRemirror({ extensions, extraAttributes });
 
@@ -39,6 +115,8 @@ export const SocialEditor: FC = (props) => {
         <Remirror manager={manager}>
           <EditorComponent />
           <EmojiPopupComponent />
+          <MentionComponent {...props} />
+          <FloatingToolbar items={floatingToolbarItems} positioner='selection' placement='bottom' />
           {children}
         </Remirror>
       </ThemeProvider>
