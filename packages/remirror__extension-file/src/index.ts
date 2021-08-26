7fa7af75e342561996255e1c9e4b17559264fa5b
Will Hawker <whawker@users.noreply.github.com>
2021-08-26 19:36:46 +0100

    fix(file): export UploadContext type (#1173)

--- packages/remirror__extension-file/src/index.ts
+++ packages/remirror__extension-file/src/index.ts
@@ -1,4 +1,5 @@
 export type { FileAttributes, FileOptions } from './file-extension';
 export { FileExtension } from './file-extension';
+export type { UploadContext } from './file-upload-context';
 export type { FileUploader } from './file-uploader';
 export * from './file-uploaders';
