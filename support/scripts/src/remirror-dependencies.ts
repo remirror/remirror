ec74319c16bc18112c51eceb262b547026a568dd
ocavue <ocavue@gmail.com>
2021-09-02 08:30:03 +0800

    add script

---
+++ support/scripts/src/remirror-dependencies.ts
@@ -0,0 +1,16 @@
+/**
+ * @script
+ *
+ * This script will make sure that the package `remirror` doesn't depend on `react`.
+ */
+
+import { getPackages } from '@manypkg/get-packages';
+
+import { baseDir, getAllDependencies } from '.';
+
+function main() {
+  packages = getPackages(baseDir());
+  console.log('packages:', packages);
+}
+
+main();
