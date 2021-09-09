334cb247242dade9f8b8f91329fc10c27e3cc258
ocavue <ocavue@gmail.com>
2021-09-09 11:01:26 +0800

    add code

---
+++ docs/examples/extension-code/basic.tsx
@@ -0,0 +1,23 @@
+/**
+ * THIS FILE IS AUTO GENERATED
+ *
+ * Run `pnpm -w generate:website-examples` to regenerate this file.
+ */
+
+// @ts-nocheck
+
+import CodeBlock from '@theme/CodeBlock';
+import ComponentSource from '!!raw-loader!../../packages/storybook-react/stories/extension-code/basic.tsx';
+
+import ComponentStory from '../../packages/storybook-react/stories/extension-code/basic';
+import { ExampleRoot } from '../components/example-root';
+
+const ExampleComponent = (): JSX.Element => {
+  const story = <ComponentStory />;
+  const source = <CodeBlock className='language-tsx'>{ComponentSource}</CodeBlock>;
+
+  return <ExampleRoot story={story} source={source} />;
+};
+
+export default ExampleComponent;
+  
---
+++ packages/storybook-react/stories/extension-code/basic.tsx
@@ -0,0 +1,44 @@
+import 'remirror/styles/all.css';
+import './styles.css';
+
+import { cx, htmlToProsemirrorNode } from 'remirror';
+import { CodeExtension } from 'remirror/extensions';
+import { ProsemirrorDevTools } from '@remirror/dev';
+import { Remirror, ThemeProvider, useActive, useCommands, useRemirror } from '@remirror/react';
+
+const extensions = () => [new CodeExtension()];
+
+const CodeButton = () => {
+  const commands = useCommands();
+  const active = useActive(true);
+  return (
+    <button onClick={() => commands.toggleCode()} className={cx(active.code() && 'active')}>
+      Code
+    </button>
+  );
+};
+
+const Basic = (): JSX.Element => {
+  const { manager, state, onChange } = useRemirror({
+    extensions: extensions,
+    content: '<p>Text as <code>code</code></p>',
+    stringHandler: htmlToProsemirrorNode,
+  });
+
+  return (
+    <ThemeProvider>
+      <Remirror
+        manager={manager}
+        autoFocus
+        onChange={onChange}
+        initialContent={state}
+        autoRender='end'
+      >
+        <CodeButton />
+        <ProsemirrorDevTools />
+      </Remirror>
+    </ThemeProvider>
+  );
+};
+
+export default Basic;
