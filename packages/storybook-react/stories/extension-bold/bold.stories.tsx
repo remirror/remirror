ec0faa013dc5056cd1b101d2a87a16c3ad1f4770
ocavue <ocavue@gmail.com>
2021-09-09 12:37:57 +0800

    update bold

--- packages/storybook-react/stories/extension-bold/bold.stories.tsx
+++ packages/storybook-react/stories/extension-bold/bold.stories.tsx
@@ -1,42 +1,5 @@
-import 'remirror/styles/all.css';
-import './styles.css';
-
-import { cx, htmlToProsemirrorNode } from 'remirror';
-import { BoldExtension } from 'remirror/extensions';
-import { Remirror, ThemeProvider, useActive, useCommands, useRemirror } from '@remirror/react';
+import { Basic } from './basic';
 
 export default { title: 'Extensions / Bold' };
 
-const extensions = () => [new BoldExtension()];
-
-const BoldButton = () => {
-  const commands = useCommands();
-  const active = useActive(true);
-  return (
-    <button onClick={() => commands.toggleBold()} className={cx(active.bold() && 'active')}>
-      Bold
-    </button>
-  );
-};
-
-export const Basic = (): JSX.Element => {
-  const { manager, state, onChange } = useRemirror({
-    extensions: extensions,
-    content: '<p>Text in <b>bold</b></p>',
-    stringHandler: htmlToProsemirrorNode,
-  });
-
-  return (
-    <ThemeProvider>
-      <Remirror
-        manager={manager}
-        autoFocus
-        onChange={onChange}
-        initialContent={state}
-        autoRender='end'
-      >
-        <BoldButton />
-      </Remirror>
-    </ThemeProvider>
-  );
-};
+export { Basic };
