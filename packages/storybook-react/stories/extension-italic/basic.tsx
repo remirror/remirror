8cc3477a196c7d429114d3a101bf10fbf80aa123
ocavue <ocavue@users.noreply.github.com>
2021-09-09 06:30:13 +0000

    update italic

---
+++ packages/storybook-react/stories/extension-italic/basic.tsx
@@ -0,0 +1,42 @@
+import 'remirror/styles/all.css';
+import './styles.css';
+
+import { cx, htmlToProsemirrorNode } from 'remirror';
+import { ItalicExtension } from 'remirror/extensions';
+import { Remirror, ThemeProvider, useActive, useCommands, useRemirror } from '@remirror/react';
+
+const extensions = () => [new ItalicExtension()];
+
+const ItalicButton = () => {
+  const commands = useCommands();
+  const active = useActive(true);
+  return (
+    <button onClick={() => commands.toggleItalic()} className={cx(active.italic() && 'active')}>
+      Italic
+    </button>
+  );
+};
+
+const Basic = (): JSX.Element => {
+  const { manager, state, onChange } = useRemirror({
+    extensions: extensions,
+    content: '<p>Text in <i>italic</i></p>',
+    stringHandler: htmlToProsemirrorNode,
+  });
+
+  return (
+    <ThemeProvider>
+      <Remirror
+        manager={manager}
+        autoFocus
+        onChange={onChange}
+        initialContent={state}
+        autoRender='end'
+      >
+        <ItalicButton />
+      </Remirror>
+    </ThemeProvider>
+  );
+};
+
+export default Basic;
