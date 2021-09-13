9b4e7751e13161ccf645fe32b14369f2f4ab1627
Ronny Roeller <ronny@collaborne.com>
2021-09-13 19:35:27 +0200

    Don't allow for IP addresses

--- packages/remirror__extension-link/src/link-extension.ts
+++ packages/remirror__extension-link/src/link-extension.ts
@@ -174,7 +174,7 @@ export type LinkAttributes = ProsemirrorAttributes<{
     openLinkOnClick: false,
     // Based on https://gist.github.com/dperini/729294
     autoLinkRegex:
-      /(?:(?:(?:https?|ftp):)?\/\/)?(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[01])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4])|(?:(?:[\da-z\u00A1-\uFFFF][\w\u00A1-\uFFFF-]{0,62})?[\da-z\u00A1-\uFFFF]\.)+[a-z\u00A1-\uFFFF]{2,}\.?)(?::\d{2,5})?(?:[#/?]\S*)?/gi,
+      /(?:(?:(?:https?|ftp):)?\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[\da-z\u00A1-\uFFFF][\w\u00A1-\uFFFF-]{0,62})?[\da-z\u00A1-\uFFFF]\.)+[a-z\u00A1-\uFFFF]{2,}\.?)(?::\d{2,5})?(?:[#/?]\S*)?/gi,
     defaultTarget: null,
     supportedTargets: [],
   },
