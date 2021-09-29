f41a1cd0e2d986599cd7705daa7248e8dfd6bb9b
Ronny Roeller <ronny@collaborne.com>
2021-09-29 11:04:10 +0200

    docs: improve docs for CodeBlockExtension

--- packages/storybook-react/stories/extension-code-block/basic.tsx
+++ packages/storybook-react/stories/extension-code-block/basic.tsx
@@ -1,16 +1,27 @@
+import css from 'refractor/lang/css';
 import javascript from 'refractor/lang/javascript';
+import json from 'refractor/lang/json';
+import markdown from 'refractor/lang/markdown';
 import typescript from 'refractor/lang/typescript';
 import { CodeBlockExtension } from 'remirror/extensions';
 import { Remirror, ThemeProvider, useRemirror } from '@remirror/react';
 
 const extensions = () => [
   new CodeBlockExtension({
-    supportedLanguages: [javascript, typescript],
+    supportedLanguages: [css, javascript, json, markdown, typescript],
   }),
 ];
 
-const content = `<pre><code data-code-block-language="typescript">function sayHello {
-  console.log('hello world!')
+const content = `
+<pre><code data-code-block-language="typescript">function sayHello {
+  console.log('Hello world, TypeScript!')
+}</code></pre>
+<pre><code data-code-block-language="markdown">Hello _world_, **Markdown**</code></pre>
+<pre><code data-code-block-language="css">.hello-world-css {
+  color: red;
+}</code></pre>
+<pre><code data-code-block-language="json">{
+  "hello-world": "JSON"
 }</code></pre>
 `;
 
