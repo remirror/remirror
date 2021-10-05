5467fc7b3a3869bf6554b0b0cb310dc191e9de0d
ocavue <ocavue@users.noreply.github.com>
2021-10-06 06:10:06 +0800

    fix(extension-list): merge patch from the upstream (#1288)
    Merge the upstream patch https://github.com/ProseMirror/prosemirror-schema-list/commit/38867345f6d97d6793655ed77c16f1a7b18f6846

--- packages/remirror__extension-list/src/list-commands.ts
+++ packages/remirror__extension-list/src/list-commands.ts
@@ -358,6 +358,8 @@ function liftOutOfList(state: EditorState, dispatch: DispatchFunction, range: No
   const tr = state.tr,
     list = range.parent;
 
+  const originMappingLength = tr.mapping.maps.length;
+
   // Merge the list items into a single big item
   for (let pos = range.end, i = range.endIndex - 1, e = range.startIndex; i > e; i--) {
     pos -= list.child(i).nodeSize;
@@ -366,14 +368,19 @@ function liftOutOfList(state: EditorState, dispatch: DispatchFunction, range: No
 
   const $start = tr.doc.resolve(range.start),
     item = $start.nodeAfter;
-  const atStart = range.startIndex === 0,
-    atEnd = range.endIndex === list.childCount;
-  const parent = $start.node(-1),
-    indexBefore = $start.index(-1);
 
   if (!item) {
     return false;
   }
+
+  if (tr.mapping.slice(originMappingLength).map(range.end) !== range.start + item.nodeSize) {
+    return false;
+  }
+
+  const atStart = range.startIndex === 0,
+    atEnd = range.endIndex === list.childCount;
+  const parent = $start.node(-1),
+    indexBefore = $start.index(-1);
 
   if (
     !parent.canReplace(
@@ -436,11 +443,7 @@ export function liftListItemOutOfList(itemType: NodeType): CommandFunction {
 function getItemRange(itemType: NodeType, selection: Selection) {
   const { $from, $to } = selection;
 
-  const range = $from.blockRange(
-    $to,
-    // @ts-expect-error this line of code is copied from `prosemirror-schema-list`
-    (node) => node.childCount && node.firstChild.type === itemType,
-  );
+  const range = $from.blockRange($to, (node) => node.firstChild?.type === itemType);
 
   return range;
 }
