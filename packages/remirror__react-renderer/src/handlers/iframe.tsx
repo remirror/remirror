1e94c9dcfc19e8a0fd11878c1a79df09117a7c1d
ocavue <ocavue@gmail.com>
2021-10-23 17:30:16 +0800

    fix a warning

--- packages/remirror__react-renderer/src/handlers/iframe.tsx
+++ packages/remirror__react-renderer/src/handlers/iframe.tsx
@@ -3,11 +3,13 @@ import { Literal, RemirrorJSON } from '@remirror/core';
 
 import { MarkMap } from '../types';
 
-export const createIFrameHandler = (overwriteAttrs?: Record<string, Literal>) => {
-  const iframeHandler: FC<{
-    node: RemirrorJSON;
-    markMap: MarkMap;
-  }> = (props) => {
+type IFrameHandler = FC<{
+  node: RemirrorJSON;
+  markMap: MarkMap;
+}>;
+
+export const createIFrameHandler = (overwriteAttrs?: Record<string, Literal>): IFrameHandler => {
+  const iframeHandler: IFrameHandler = (props) => {
     // Remove remirror-internal attributes
     const { allowFullScreen, type, ...attrs } = props.node.attrs ?? {};
     const normalizedAttrs = {
