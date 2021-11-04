304926173a2b7203cfc1122d14dc52740a5753fa
ocavue <ocavue@gmail.com>
2021-11-04 12:27:53 +0800

    add delay for debug

--- packages/storybook-react/stories/extension-image/resizable.tsx
+++ packages/storybook-react/stories/extension-image/resizable.tsx
@@ -1,7 +1,6 @@
 import 'remirror/styles/all.css';
 
 import React from 'react';
-import { htmlToProsemirrorNode } from 'remirror';
 import { ImageExtension } from 'remirror/extensions';
 import { Remirror, ThemeProvider, useRemirror } from '@remirror/react';
 
@@ -10,10 +9,40 @@ const extensions = () => [new ImageExtension({ enableResizing: true })];
 const Resizable: React.FC = () => {
   const { manager, state, onChange } = useRemirror({
     extensions,
-    content:
-      '<p>You can see a resizable image below. Move your mouse over the image and drag the resizing handler to resize it.</p>' +
-      '<p style="display:flex; align-items: center; justify-content: center;"><img src="https://dummyimage.com/200x80/479e0c/fafafa"></div>',
-    stringHandler: htmlToProsemirrorNode,
+    content: {
+      type: 'doc',
+      content: [
+        {
+          type: 'paragraph',
+          content: [
+            {
+              type: 'text',
+              text: 'You can see a resizable image below. Move your mouse over the image and drag the resizing handler to resize it.',
+            },
+          ],
+        },
+        {
+          type: 'paragraph',
+          content: [
+            {
+              type: 'image',
+              attrs: {
+                alt: '',
+                crop: null,
+                height: 160,
+                width: 400,
+                rotate: null,
+                src: 'https://deelay.me/10000/https://dummyimage.com/2000x800/479e0c/fafafa',
+                title: '',
+                fileName: null,
+                resizable: false,
+              },
+            },
+          ],
+        },
+        { type: 'paragraph', content: [{ type: 'text', text: 'aa' }] },
+      ],
+    },
   });
 
   return (
