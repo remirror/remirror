466d6bf2065ad25e74d565ac6bd927d3599288e2
ocavue <ocavue@gmail.com>
2021-11-11 16:43:58 +0800

    add test cases

--- packages/storybook-react/stories/extension-link/with-telephone-support.tsx
+++ packages/storybook-react/stories/extension-link/with-telephone-support.tsx
@@ -3,13 +3,12 @@ import { Remirror, ThemeProvider, useRemirror } from '@remirror/react';
 
 const phoneRegex = /(?:\+?(\d{1,3}))?[ (.-]*(\d{3})[ ).-]*(\d{3})[ .-]*(\d{4})(?: *x(\d+))?/;
 const linkRegex =
-  /(?:(?:(?:https?|ftp):)?\/\/)?(?:\S+(?::\S*)?@)?(?:(?:[\da-z\u00A1-\uFFFF][\w\u00A1-\uFFFF-]{0,62})?[\da-z\u00A1-\uFFFF]\.)+[a-z\u00A1-\uFFFF]{2,}\.?(?::\d{2,5})?(?:[#/?]\S*)?/gi;
-
+  /(?:(?:(?:https?|ftp):)?\/\/)?(?:\S+(?::\S*)?@)?(?:(?:[\da-z\u00A1-\uFFFF][\w\u00A1-\uFFFF-]{0,62})?[\da-z\u00A1-\uFFFF]\.)+[a-z\u00A1-\uFFFF]{2,}\.?(?::\d{2,5})?(?:[#/?]\S*)?/i;
 const composedRegex = new RegExp(
   [phoneRegex, linkRegex].map((regex) => `(${regex.source})`).join('|'),
 );
 
-function extractHref({
+function extractLinkOrTel({
   url,
   defaultProtocol,
 }: {
@@ -23,7 +22,11 @@ function extractHref({
 const WithTelephoneSupport = (): JSX.Element => {
   const { manager, state } = useRemirror({
     extensions: () => [
-      new LinkExtension({ autoLink: true, autoLinkRegex: composedRegex, extractHref }),
+      new LinkExtension({
+        autoLink: true,
+        autoLinkRegex: composedRegex,
+        extractHref: extractLinkOrTel,
+      }),
     ],
     content: `Type "800-555-1234" to insert a phone:&nbsp;`,
     stringHandler: 'html',
