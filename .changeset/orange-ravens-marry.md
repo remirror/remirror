6768e5e9921fe6e6eb666850f2e76d4e72b899d6
ocavue <ocavue@gmail.com>
2021-11-11 15:52:34 +0800

    feat(extension-link): add a new extension option `extractHref`

---
+++ .changeset/orange-ravens-marry.md
@@ -0,0 +1,6 @@
+---
+'@remirror/extension-link': patch
+---
+
+Add a new option `extractHref` to `ExtensionLink`. Users can use this option to customize the `href` attribute, for example `file://` and `tel`.
+
---
+++ packages/storybook-react/stories/extension-link/with-telephone-support.tsx
@@ -0,0 +1,39 @@
+import { DefaultProtocol, extractHref as extractLink, LinkExtension } from 'remirror/extensions';
+import { Remirror, ThemeProvider, useRemirror } from '@remirror/react';
+
+const phoneRegex = /(?:\+?(\d{1,3}))?[ (.-]*(\d{3})[ ).-]*(\d{3})[ .-]*(\d{4})(?: *x(\d+))?/;
+const linkRegex =
+  /(?:(?:(?:https?|ftp):)?\/\/)?(?:\S+(?::\S*)?@)?(?:(?:[\da-z\u00A1-\uFFFF][\w\u00A1-\uFFFF-]{0,62})?[\da-z\u00A1-\uFFFF]\.)+[a-z\u00A1-\uFFFF]{2,}\.?(?::\d{2,5})?(?:[#/?]\S*)?/gi;
+
+const composedRegex = new RegExp(
+  [phoneRegex, linkRegex].map((regex) => `(${regex.source})`).join('|'),
+);
+
+function extractHref({
+  url,
+  defaultProtocol,
+}: {
+  url: string;
+  defaultProtocol: DefaultProtocol;
+}): string {
+  const isLink = linkRegex.test(url);
+  return isLink ? extractLink({ url, defaultProtocol }) : `tel:${url}`;
+}
+
+const WithTelephoneSupport = (): JSX.Element => {
+  const { manager, state } = useRemirror({
+    extensions: () => [
+      new LinkExtension({ autoLink: true, autoLinkRegex: composedRegex, extractHref }),
+    ],
+    content: `Type "800-555-1234" to insert a phone:&nbsp;`,
+    stringHandler: 'html',
+  });
+
+  return (
+    <ThemeProvider>
+      <Remirror manager={manager} initialContent={state} />
+    </ThemeProvider>
+  );
+};
+
+export default WithTelephoneSupport;
