ca2e8d71dd8c1535c73376fb9712b8c5b9702931
Andreas Kohn <andreas.kohn@gmail.com>
2022-01-27 18:06:43 +0100

    docs: include controls for adding/removing annotations in the yjs extension story

--- packages/storybook-react/stories/extension-yjs/basic.tsx
+++ packages/storybook-react/stories/extension-yjs/basic.tsx
@@ -3,7 +3,14 @@ import 'remirror/styles/all.css';
 import { AnnotationExtension, PlaceholderExtension, YjsExtension } from 'remirror/extensions';
 import { WebrtcProvider } from 'y-webrtc';
 import * as Y from 'yjs';
-import { Remirror, ThemeProvider, useRemirror } from '@remirror/react';
+import {
+  Remirror,
+  ThemeProvider,
+  useCommands,
+  useCurrentSelection,
+  useHelpers,
+  useRemirror,
+} from '@remirror/react';
 
 const ydoc = new Y.Doc();
 // clients connected to the same room-name share document updates
@@ -15,12 +22,44 @@ const extensions = () => [
   new YjsExtension({ getProvider: () => provider }),
 ];
 
+const Menu = () => {
+  const { removeAnnotations, addAnnotation } = useCommands();
+  const { getAnnotationsAt, selectionHasAnnotation } = useHelpers();
+  const selection = useCurrentSelection();
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          addAnnotation({ id: `${Date.now()}` });
+          focus();
+        }}
+        disabled={selection.empty}
+      >
+        Add annotation
+      </button>
+      <button
+        onClick={() => {
+          const annotations = getAnnotationsAt(selection.from);
+          removeAnnotations(annotations.map(({ id }) => id));
+          focus();
+        }}
+        disabled={!selectionHasAnnotation()}
+      >
+        Remove annotation(s)
+      </button>
+    </>
+  );
+};
+
 const Basic = (): JSX.Element => {
   const { manager } = useRemirror({ extensions, core: { excludeExtensions: ['history'] } });
 
   return (
     <ThemeProvider>
-      <Remirror manager={manager} autoFocus autoRender='end' />
+      <Remirror manager={manager} autoFocus autoRender='end'>
+        <Menu />
+      </Remirror>
     </ThemeProvider>
   );
 };
