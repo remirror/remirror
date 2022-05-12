84c9b9a2bff533b562cae1c649bdc8ad072f545e
Will Hawker <w.hawker@hotmail.co.uk>
2022-05-12 16:01:08 +0100

    chore(storybook): add debug story for visualizing paste buffer

--- packages/storybook-react/preview.js
+++ packages/storybook-react/preview.js
@@ -3,7 +3,7 @@ import './storybook.css';
 export const parameters = {
   options: {
     storySort: {
-      order: ['Editors', 'Extensions', 'React Hooks'],
+      order: ['Editors', 'Extensions', 'React Hooks', 'Components (labs)', 'Utils'],
     },
   },
 };
