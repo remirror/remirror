a6b001f534586372702efe318c62a0e13d3cc8d5
ocavue <ocavue@gmail.com>
2022-06-01 17:19:31 +0800

    wip

--- packages/remirror__core/src/builtins/node-views-extension.ts
+++ packages/remirror__core/src/builtins/node-views-extension.ts
@@ -45,7 +45,6 @@ export class NodeViewsExtension extends PlainExtension {
     }
 
     return {
-      // @ts-expect-error: the `nodeViews` seems to get the wrong type. https://discuss.prosemirror.net/t/prosemirror-is-now-a-typescript-project/4624/26
       props: { nodeViews },
     };
   }
