55c2498967cfc3e7dd262d7da7609fe332fe9e76
ocavue <ocavue@gmail.com>
2022-06-12 18:25:33 +0800

    watch

--- packages/remirror__cli/src/index.ts
+++ packages/remirror__cli/src/index.ts
@@ -1,6 +1,7 @@
 import { Command } from 'commander';
 
 import { build } from './commands/build';
+import { watch } from './commands/watch';
 
 export async function main() {
   const program = new Command();
@@ -15,6 +16,15 @@ export async function main() {
 Notice that this command will not bundle .d.ts files for you nor check TypeScript typings. You will need to run "pnpm -w typecheck" to do that.`,
     )
     .action(build);
+
+  program
+    .command('watch')
+    .description(
+      `Watch files and build NPM packages when files change in current monorepo.
+
+Notice that this command will not bundle .d.ts files for you nor check TypeScript typings. You will need to run "pnpm -w typecheck" to do that.`,
+    )
+    .action(watch);
 
   program.parse();
 }
--- packages/remirror__cli/src/utils/get-root.ts
+++ packages/remirror__cli/src/utils/get-root.ts
@@ -7,7 +7,7 @@ let root = '';
 /**
  * Get the absolute path to the root of the monorepo.
  */
-export function getRoot() {
+export function getRoot(): string {
   if (!root) {
     root = path.resolve(__dirname, '../../../..');
     logger.debug(`project root is ${root}`);
