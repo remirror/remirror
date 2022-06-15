f8bbcc32fb160de242962e4e80b59aa31d7b66aa
ocavue <ocavue@gmail.com>
2022-06-15 13:42:30 +0800

    works

--- packages/storybook-react2/.storybook/main.js
+++ packages/storybook-react2/.storybook/main.js
@@ -81,23 +81,23 @@ module.exports = {
     storyStoreV7: true,
   },
 
-  webpackFinal: async (config, { configType }) => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
+  // webpackFinal: async (config, { configType }) => {
+  //   // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+  //   // You can change the configuration based on that.
+  //   // 'PRODUCTION' is used when building the static version of storybook.
 
-    // Disable fullySpecified
-    config.resolve.fullySpecified = false;
-    // console.log('config:', config);
+  //   // Disable fullySpecified
+  //   config.resolve.fullySpecified = false;
+  //   // console.log('config:', config);
 
-    // // Make whatever fine-grained changes you need
-    // config.module.rules.push({
-    //   test: /\.scss$/,
-    //   use: ['style-loader', 'css-loader', 'sass-loader'],
-    //   include: path.resolve(__dirname, '../'),
-    // });
+  //   // // Make whatever fine-grained changes you need
+  //   // config.module.rules.push({
+  //   //   test: /\.scss$/,
+  //   //   use: ['style-loader', 'css-loader', 'sass-loader'],
+  //   //   include: path.resolve(__dirname, '../'),
+  //   // });
 
-    // Return the altered config
-    return config;
-  },
+  //   // Return the altered config
+  //   return config;
+  // },
 };
