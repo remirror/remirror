6fab89bbb1da207218f9797c216b382d05fa3d66
ocavue <ocavue@gmail.com>
2022-06-15 12:50:34 +0800

    fix icons

--- packages/remirror__extension-file/src/file-icons.tsx
+++ packages/remirror__extension-file/src/file-icons.tsx
@@ -41,7 +41,7 @@ export const DeleteIcon: React.FC = () => (
     xmlns='http://www.w3.org/2000/svg'
   >
     <path fill='none' d='M0 0h24v24H0z' />
-    <path d='M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z' />
+    <path d='M7 4V2h10v2h5v2h-2v15a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V6H2V4h5zM6 6v14h12V6H6zm3 3h2v8H9V9zm4 0h2v8h-2V9z' />
   </svg>
 );
 
