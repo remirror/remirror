2a95942625a2e51a50bad70e30cc942aa1bb43cd
ocavue <ocavue@gmail.com>
2022-06-15 13:41:32 +0800

    preview

---
+++ packages/storybook-react2/.storybook/preview.js
@@ -0,0 +1,9 @@
+import './storybook.css';
+
+export const parameters = {
+  options: {
+    storySort: {
+      order: ['Editors', 'Extensions', 'React Hooks', 'Components (labs)', 'Utils'],
+    },
+  },
+};
