9a78ebd0a1dc1e9504981a0497c2a79030d892e4
ocavue <ocavue@gmail.com>
2022-06-15 17:25:16 +0800

    Disable '@docusaurus/preset-classic'

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -95,35 +95,35 @@ module.exports = {
       darkTheme: darkCodeTheme,
     },
   },
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          path: path.join(__dirname, '../docs'),
-          sidebarPath: require.resolve('./sidebar.js'),
-          editUrl: ({ version, versionDocsDirPath, docPath, permalink, locale }) => {
-            // API documents are generated, so they won't have urls for directly edit.
-            if (permalink.startsWith('/docs/api')) {
-              return;
-            }
+  // presets: [
+  //   [
+  //     '@docusaurus/preset-classic',
+  //     {
+  //       docs: {
+  //         path: path.join(__dirname, '../docs'),
+  //         sidebarPath: require.resolve('./sidebar.js'),
+  //         editUrl: ({ version, versionDocsDirPath, docPath, permalink, locale }) => {
+  //           // API documents are generated, so they won't have urls for directly edit.
+  //           if (permalink.startsWith('/docs/api')) {
+  //             return;
+  //           }
 
-            return 'https://github.com/remirror/remirror/edit/HEAD/website/';
-          },
-        },
-        blog: {
-          showReadingTime: true,
-          editUrl: 'https://github.com/remirror/remirror/edit/HEAD/website/blog/',
-        },
-        theme: {
-          customCss: [require.resolve('./styles.css'), require.resolve('remirror/styles/all.css')],
-        },
-        googleAnalytics: { trackingID: 'UA-135738542-1', anonymizeIP: true },
-      },
-    ],
-  ],
+  //           return 'https://github.com/remirror/remirror/edit/HEAD/website/';
+  //         },
+  //       },
+  //       blog: {
+  //         showReadingTime: true,
+  //         editUrl: 'https://github.com/remirror/remirror/edit/HEAD/website/blog/',
+  //       },
+  //       theme: {
+  //         customCss: [require.resolve('./styles.css'), require.resolve('remirror/styles/all.css')],
+  //       },
+  //       googleAnalytics: { trackingID: 'UA-135738542-1', anonymizeIP: true },
+  //     },
+  //   ],
+  // ],
   plugins: [
-    path.join(__dirname, 'plugins', 'basic-plugin.js'),
+    // path.join(__dirname, 'plugins', 'basic-plugin.js'),
     require.resolve('@docusaurus/plugin-ideal-image'),
     // [
     //   'docusaurus-plugin-examples',
