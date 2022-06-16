a5b4531ff1d402174233b11072c913571526c24b
ocavue <ocavue@gmail.com>
2022-06-16 17:27:29 +0800

    try to remove case-anything from @remirror/theme

--- packages/remirror__theme/src/utils.ts
+++ packages/remirror__theme/src/utils.ts
@@ -61,7 +61,7 @@ export type RemirrorThemeType = DeepPartial<Remirror.Theme>;
 
 function simpleKebabCase(str: string) {
   return str
-    .replace(/([a-z])([A-Z])/g, '$1-$2')
+    .replace(/([a-z])([\dA-Z])/g, '$1-$2')
     .replace(/[\s_]+/g, '-')
     .toLowerCase();
 }
