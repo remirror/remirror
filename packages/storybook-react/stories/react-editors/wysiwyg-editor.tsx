f80874fbe6e865a707720136e12625bac0024923
ocavue <ocavue@gmail.com>
2022-07-08 23:22:59 +0800

    docs: bring showcases back

---
+++ packages/storybook-react/stories/react-editors/wysiwyg-editor.tsx
@@ -0,0 +1,47 @@
+import React, { useCallback } from 'react';
+import { useHelpers, useRemirrorContext } from '@remirror/react';
+import { WysiwygEditor } from '@remirror/react-editors/wysiwyg';
+
+const SAMPLE_DOC = {
+  type: 'doc',
+  content: [
+    {
+      type: 'paragraph',
+      attrs: { dir: null, ignoreBidiAutoUpdate: null },
+      content: [{ type: 'text', text: 'Loaded content' }],
+    },
+  ],
+};
+
+function LoadButton() {
+  const { setContent } = useRemirrorContext();
+  const handleClick = useCallback(() => setContent(SAMPLE_DOC), [setContent]);
+
+  return (
+    <button onMouseDown={(event) => event.preventDefault()} onClick={handleClick}>
+      Load
+    </button>
+  );
+}
+
+function SaveButton() {
+  const { getJSON } = useHelpers();
+  const handleClick = useCallback(() => alert(JSON.stringify(getJSON())), [getJSON]);
+
+  return (
+    <button onMouseDown={(event) => event.preventDefault()} onClick={handleClick}>
+      Save
+    </button>
+  );
+}
+
+const Basic: React.FC = () => {
+  return (
+    <WysiwygEditor placeholder='Start typing...'>
+      <LoadButton />
+      <SaveButton />
+    </WysiwygEditor>
+  );
+};
+
+export default Basic;
---
+++ website/extension-examples/react-editors/wysiwyg-editor.tsx
@@ -0,0 +1,31 @@
+/**
+ * THIS FILE IS AUTO GENERATED!
+ *
+ * Run `pnpm -w generate:website-examples` to regenerate this file.
+ */
+
+// @ts-nocheck
+
+import React from 'react';
+import CodeBlock from '@theme/CodeBlock';
+import BrowserOnly from '@docusaurus/BrowserOnly';
+import ComponentSource from '!!raw-loader!../../../packages/storybook-react/stories/react-editors/wysiwyg-editor.tsx';
+
+import { StoryExample } from '../../src/components/story-example-component';
+
+const ExampleComponent = (): JSX.Element => {
+  const story = (
+    <BrowserOnly>
+      {() => {
+        const ComponentStory = require('../../../packages/storybook-react/stories/react-editors/wysiwyg-editor').default
+        return <ComponentStory/>
+      }}
+    </BrowserOnly>
+  );
+  const source = <CodeBlock className='language-tsx'>{ComponentSource}</CodeBlock>;
+
+  return <StoryExample story={story} source={source} />;
+};
+
+export default ExampleComponent;
+  
