4c5c57d694722ff7a2ed2ea89d7577d3d18778cc
ocavue <ocavue@users.noreply.github.com>
2022-07-11 23:12:34 +0800

    chore(deps): update prosemirror packages (#1779)

--- packages/remirror__pm/src/tables.ts
+++ packages/remirror__pm/src/tables.ts
@@ -1,13 +1,11 @@
 import { CellSelection } from 'prosemirror-tables';
 
-import { EditorSchema } from './extra/pm-types';
-
 /**
  * Predicate checking whether the selection is a [[`CellSelection`]].
  *
  * @param value - the value to check
  */
-export function isCellSelection(value: unknown): value is CellSelection<EditorSchema> {
+export function isCellSelection(value: unknown): value is CellSelection {
   return typeof value === 'object' && value instanceof CellSelection;
 }
 
