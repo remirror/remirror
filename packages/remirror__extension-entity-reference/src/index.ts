c33ed987b362ffcf9871bf8ae3e7b03e1bc2d8f4
Ronny Roeller <ronny@nextapp.co>
2022-07-12 12:15:22 +0200

    fix: export entity-reference positioner
    There is a positioner for entity-reference-extension but it's not properly exported. This makes it difficult to use.
    
    See here an example from annotation-extension that does export the positioner: https://github.com/remirror/remirror/blob/1bc49d747b693146bb320017de506aa92efcbc74/packages/remirror__extension-annotation/src/index.ts#L2

--- packages/remirror__extension-entity-reference/src/index.ts
+++ packages/remirror__extension-entity-reference/src/index.ts
@@ -1,3 +1,4 @@
 export * from './entity-reference-extension';
+export { centeredEntityReferencePositioner } from './entity-reference-positioners';
 export type { EntityReferenceMetaData } from './types';
 export { findMinMaxRange } from './utils/ranges';
