916b1074dbe3fe159f73f08f1d2a1544cc97c82c
Ronny Roeller <ronny@nextapp.co>
2022-07-12 12:31:45 +0200

    feat: add helper to get shortest entity reference
    This helper can be used when reacting to clicks on overlapping highlights. In that case, the app should show the shortest entity because longer entities typical have other click areas.

--- packages/remirror__extension-entity-reference/src/index.ts
+++ packages/remirror__extension-entity-reference/src/index.ts
@@ -2,3 +2,4 @@ export * from './entity-reference-extension';
 export { centeredEntityReferencePositioner } from './entity-reference-positioners';
 export type { EntityReferenceMetaData } from './types';
 export { findMinMaxRange } from './utils/ranges';
+export { getShortestEntityReference } from './utils/shortest-entity-reference';
