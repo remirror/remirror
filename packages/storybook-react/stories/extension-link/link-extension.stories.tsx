c06663f20da110763d3d52748d2a465051926f5b
marcoSven <me@marcosven.com>
2022-09-09 05:58:00 +0200

    docs: add linkify example

--- packages/storybook-react/stories/extension-link/link-extension.stories.tsx
+++ packages/storybook-react/stories/extension-link/link-extension.stories.tsx
@@ -1,7 +1,8 @@
 import Basic from './basic';
 import ClickHandler from './click-handler';
 import EditDialog from './edit-dialog';
+import Linkify from './linkify';
 import WithTelephoneSupport from './with-telephone-support';
 
-export { Basic, ClickHandler, EditDialog, WithTelephoneSupport };
+export { Basic, ClickHandler, EditDialog, Linkify, WithTelephoneSupport };
 export default { title: 'Extensions / Link' };
