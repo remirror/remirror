8dde13830c6dc45836627e34d5d65065e49a0c15
Will Hawker <w.hawker@hotmail.co.uk>
2022-09-24 22:53:44 +0100

    wip: polymorphic components test

--- packages/remirror__react-components/src/menus/toggle-callout-menu-item.tsx
+++ packages/remirror__react-components/src/menus/toggle-callout-menu-item.tsx
@@ -1,34 +1,10 @@
-import React, { FC, useCallback } from 'react';
-import { CalloutExtension, CalloutExtensionAttributes } from 'remirror/extensions';
-import { useActive, useCommands } from '@remirror/react-core';
+import React, { FC } from 'react';
 
-import { CommandMenuItem, CommandMenuItemProps } from './command-menu-item';
+import { ToggleCallout, ToggleCalloutProps } from '../commands/toggle-callout';
+import { CommandMenuItem } from './command-menu-item';
 
-export interface ToggleCalloutMenuItemProps
-  extends Omit<CommandMenuItemProps, 'commandName' | 'active' | 'enabled' | 'attrs' | 'onSelect'> {
-  attrs?: Partial<CalloutExtensionAttributes>;
-}
+export type ToggleCalloutMenuItemProps = Omit<ToggleCalloutProps<typeof CommandMenuItem>, 'as'>;
 
-export const ToggleCalloutMenuItem: FC<ToggleCalloutMenuItemProps> = ({ attrs, ...rest }) => {
-  const { toggleCallout } = useCommands<CalloutExtension>();
-
-  const handleSelect = useCallback(() => {
-    if (toggleCallout.enabled(attrs)) {
-      toggleCallout(attrs);
-    }
-  }, [toggleCallout, attrs]);
-
-  const active = useActive<CalloutExtension>().callout(attrs);
-  const enabled = toggleCallout.enabled(attrs);
-
-  return (
-    <CommandMenuItem
-      {...rest}
-      commandName='toggleCallout'
-      active={active}
-      enabled={enabled}
-      attrs={attrs}
-      onSelect={handleSelect}
-    />
-  );
+export const ToggleCalloutMenuItem: FC<ToggleCalloutMenuItemProps> = (props) => {
+  return <ToggleCallout as={CommandMenuItem} {...props} />;
 };
