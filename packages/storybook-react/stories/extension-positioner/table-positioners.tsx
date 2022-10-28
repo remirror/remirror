eb1b3078d41eb6b757d8efa354e2bef8d84729ad
Will Hawker <w.hawker@hotmail.co.uk>
2022-10-28 10:09:57 +0100

    feat(tables): add column, row, all cell selection positioners

--- packages/storybook-react/stories/extension-positioner/table-positioners.tsx
+++ packages/storybook-react/stories/extension-positioner/table-positioners.tsx
@@ -4,14 +4,17 @@ import './styles.css';
 import { css } from '@emotion/css';
 import { Placement } from '@popperjs/core';
 import { Annotations, BaseStory } from '@storybook/addons';
-import React, { useEffect } from 'react';
+import React, { ReactElement, useEffect } from 'react';
 import type { Positioner } from 'remirror/extensions';
 import {
   activeCellColumnPositioner,
   activeCellPositioner,
   activeCellRowPositioner,
+  allCellSelectionPositioner,
   allColumnsStartPositioner,
   allRowsStartPositioner,
+  cellColumnSelectionPositioner,
+  cellRowSelectionPositioner,
   cellSelectionPositioner,
   selectedColumnPositioner,
   selectedRowPositioner,
@@ -103,6 +106,7 @@ const MultiPositionerIllustration = ({ positioner }: PositionerIllustrationProps
 interface TemplateArgs {
   positioner: Positioner;
   placement: Placement;
+  content: ReactElement;
   label: string;
   multi?: boolean;
 }
@@ -115,7 +119,13 @@ const TABLE_HTML = `
   </table>
 `;
 
-const Template: Story = ({ positioner, placement, label, multi = false }: TemplateArgs) => {
+const Template: Story = ({
+  content,
+  positioner,
+  placement,
+  label,
+  multi = false,
+}: TemplateArgs) => {
   const { manager, state, onChange } = useRemirror({
     stringHandler: 'html',
     content: TABLE_HTML,
@@ -127,6 +137,7 @@ const Template: Story = ({ positioner, placement, label, multi = false }: Templa
 
   return (
     <ThemeProvider>
+      {content}
       <p>
         <strong className={css('color: var(--rmr-hue-red-9);')}>☐</strong> outline demonstrates the
         bounds of the positioner, and is shown for illustrative purposes only.
@@ -149,49 +160,66 @@ export const Table = Template.bind({});
 Table.args = {
   positioner: tablePositioner,
   placement: 'bottom',
-  label: 'Creates a rect which wraps the current table.',
+  content: <p>Creates a rect which wraps the current table.</p>,
+  label: 'The bounds of the table',
 };
 
 export const ActiveCell: Story = Template.bind({});
 ActiveCell.args = {
   positioner: activeCellPositioner,
   placement: 'bottom',
-  label: 'Creates a rect for the active table cell.',
+  content: <p>Creates a rect for the active table cell.</p>,
+  label: 'The active cell',
 };
 
 export const ActiveCellColumn: Story = Template.bind({});
 ActiveCellColumn.args = {
   positioner: activeCellColumnPositioner,
   placement: 'right',
-  label: 'Creates a rect for the column of the active cell.',
+  content: <p>Creates a rect for the column of the active cell.</p>,
+  label: 'The active column',
 };
 
 export const ActiveCellRow: Story = Template.bind({});
 ActiveCellRow.args = {
   positioner: activeCellRowPositioner,
   placement: 'bottom',
-  label: 'Creates a rect for the row of the active cell.',
+  content: <p>Creates a rect for the row of the active cell.</p>,
+  label: 'The active row',
 };
 
 export const SelectedColumn: Story = Template.bind({});
 SelectedColumn.args = {
   positioner: selectedColumnPositioner,
   placement: 'right',
-  label: 'Creates a rect for a single column where all rows are selected.',
+  content: (
+    <p>
+      Creates a rect for a <em>single</em> column cell selection which goes all the way from the top
+      to the bottom of the table.
+    </p>
+  ),
+  label: 'A single column selected',
 };
 
 export const SelectedRow: Story = Template.bind({});
 SelectedRow.args = {
   positioner: selectedRowPositioner,
   placement: 'bottom',
-  label: 'Creates a rect for a single row where all columns are selected.',
+  content: (
+    <p>
+      Creates a rect for a <em>single</em> row cell selection which goes all the way from the left
+      to the right of the table.
+    </p>
+  ),
+  label: 'A single row selected',
 };
 
 export const AllColumnsStart: Story = Template.bind({});
 AllColumnsStart.args = {
   positioner: allColumnsStartPositioner,
   placement: 'bottom',
-  label: 'Creates multiple rects for the top side of each column in a table.',
+  content: <p>Creates multiple rects for the top side of each column in a table.</p>,
+  label: 'Alternating colours for each rect',
   multi: true,
 };
 
@@ -199,7 +227,8 @@ export const AllRowsStart: Story = Template.bind({});
 AllRowsStart.args = {
   positioner: allRowsStartPositioner,
   placement: 'right',
-  label: 'Creates multiple rects for the left side of each row in a table.',
+  content: <p>Creates multiple rects for the left side of each row in a table.</p>,
+  label: 'Alternating colours for each rect',
   multi: true,
 };
 
@@ -207,7 +236,42 @@ export const CellSelection: Story = Template.bind({});
 CellSelection.args = {
   positioner: cellSelectionPositioner,
   placement: 'bottom',
-  label: 'Creates a rect which wraps the selected cells.',
+  content: <p>Creates a rect which wraps the selected cells.</p>,
+  label: 'A cell selection',
+};
+
+export const CellColumnSelection: Story = Template.bind({});
+CellColumnSelection.args = {
+  positioner: cellColumnSelectionPositioner,
+  placement: 'right',
+  content: (
+    <p>
+      Creates a rect for when the cell selection goes all the way from the top to the bottom of the
+      table.
+    </p>
+  ),
+  label: 'A column selection',
+};
+
+export const CellRowSelection: Story = Template.bind({});
+CellRowSelection.args = {
+  positioner: cellRowSelectionPositioner,
+  placement: 'bottom',
+  content: (
+    <p>
+      Creates a rect for when the cell selection goes all the way from the left to the right of the
+      table.
+    </p>
+  ),
+  label: 'A row selection',
+};
+
+export const AllCellSelection: Story = Template.bind({});
+AllCellSelection.args = {
+  positioner: allCellSelectionPositioner,
+  placement: 'bottom',
+  content: <p>Creates a rect for when all cells in a table are selected.</p>,
+  label: 'All cells selected',
 };
 
 export default Table;
