e2b133d5122b8b48e771d10093cf70cece489837
Will Hawker <w.hawker@hotmail.co.uk>
2022-11-15 16:49:12 +0000

    feat(entity-reference) add an `onClick` handler

--- packages/remirror__extension-entity-reference/src/index.ts
+++ packages/remirror__extension-entity-reference/src/index.ts
@@ -1,5 +1,5 @@
 export * from './entity-reference-extension';
 export { centeredEntityReferencePositioner } from './entity-reference-positioners';
-export type { EntityReferenceMetaData } from './types';
+export type { EntityReferenceAttributes, EntityReferenceMetaData } from './types';
 export { findMinMaxRange } from './utils/ranges';
 export { getShortestEntityReference } from './utils/shortest-entity-reference';
