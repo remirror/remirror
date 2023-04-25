504efed865b5c51e387f19ac7f1e1d60555486e5
ocavue <ocavue@gmail.com>
2023-04-26 03:01:02 +0800

    Update storybook to v7

--- packages/storybook-react/.storybook/main.js
+++ packages/storybook-react/.storybook/main.js
@@ -1,13 +1,7 @@
 module.exports = {
   stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  core: {
-    builder: 'webpack5',
-    options: {
-      lazyCompilation: true,
-    },
-  },
-  features: {
-    storyStoreV7: true,
+  framework: {
+    name: '@storybook/react-vite',
   },
 };
