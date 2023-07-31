149511224a0cdbf9d99fbb6aa4d3873e592f551b
신수형 suhshin <sushin.bot@gmail.com>
2023-07-31 21:09:58 +0900

    chore(deps): update dependency prettier to v3 (#2139)

--- packages/remirror__extension-code-block/src/formatter.ts
+++ packages/remirror__extension-code-block/src/formatter.ts
@@ -5,28 +5,11 @@
  * `@remirror/extension-code-block/formatter`.
  */
 
+import Prettier from '@prettier/sync';
 import type { BuiltInParserName, CursorOptions, CursorResult } from 'prettier';
-import babelPlugin from 'prettier/parser-babel';
-import graphqlPlugin from 'prettier/parser-graphql';
-import htmlPlugin from 'prettier/parser-html';
-import markdownPlugin from 'prettier/parser-markdown';
-import cssPlugin from 'prettier/parser-postcss';
-import typescriptPlugin from 'prettier/parser-typescript';
-import yamlPlugin from 'prettier/parser-yaml';
-import { default as Prettier } from 'prettier/standalone';
 
 import type { FormattedContent, FormatterProps } from './code-block-types';
 
-// TODO load this asynchronously
-const plugins = [
-  babelPlugin,
-  htmlPlugin,
-  typescriptPlugin,
-  markdownPlugin,
-  graphqlPlugin,
-  cssPlugin,
-  yamlPlugin,
-];
 const options: Partial<CursorOptions> = {
   bracketSpacing: true,
   arrowParens: 'always',
@@ -55,11 +38,10 @@ interface FormatCodeProps {
 /**
  * Wrapper around the prettier formatWithCursor.
  */
-function formatCode({ parser, source, cursorOffset }: FormatCodeProps) {
+function formatCode({ parser, source, cursorOffset }: FormatCodeProps): CursorResult {
   return Prettier.formatWithCursor(source, {
     ...options,
     cursorOffset,
-    plugins,
     parser,
   });
 }
--- packages/remirror__styles/src/emotion.tsx
+++ packages/remirror__styles/src/emotion.tsx
@@ -39,8 +39,11 @@ export const componentsStyledCss: ReturnType<typeof css> = css`
     white-space: nowrap;
     color: var(--rmr-color-text);
     background-color: var(--rmr-color-background);
-    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
-      border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+    transition:
+      color 0.15s ease-in-out,
+      background-color 0.15s ease-in-out,
+      border-color 0.15s ease-in-out,
+      box-shadow 0.15s ease-in-out;
     font-size: 100%;
   }
 
@@ -406,7 +409,9 @@ export const componentsStyledCss: ReturnType<typeof css> = css`
   }
 
   .remirror-animated-popover {
-    transition: opacity 250ms ease-in-out, transform 250ms ease-in-out;
+    transition:
+      opacity 250ms ease-in-out,
+      transform 250ms ease-in-out;
     opacity: 0;
     transform-origin: top center;
     transform: translate3d(0, -20px, 0);
@@ -3582,7 +3587,9 @@ export const extensionEmojiStyledCss: ReturnType<typeof css> = css`
     padding-bottom: 8px;
     margin: 0 auto;
     border-radius: 8px;
-    box-shadow: hsla(205, 70%, 15%, 0.25) 0 4px 8px, hsla(205, 70%, 15%, 0.31) 0px 0px 1px;
+    box-shadow:
+      hsla(205, 70%, 15%, 0.25) 0 4px 8px,
+      hsla(205, 70%, 15%, 0.31) 0px 0px 1px;
     background-color: white;
     z-index: 10;
     max-height: 250px;
@@ -3865,7 +3872,9 @@ export const extensionMentionAtomStyledCss: ReturnType<typeof css> = css`
     padding-bottom: 8px;
     margin: 0 auto;
     border-radius: 8px;
-    box-shadow: hsla(205, 70%, 15%, 0.25) 0 4px 8px, hsla(205, 70%, 15%, 0.31) 0px 0px 1px;
+    box-shadow:
+      hsla(205, 70%, 15%, 0.25) 0 4px 8px,
+      hsla(205, 70%, 15%, 0.31) 0px 0px 1px;
     background-color: white;
     z-index: 10;
     max-height: 250px;
